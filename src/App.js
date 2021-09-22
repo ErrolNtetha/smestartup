929b8327b649f3af0b84c998b6303f048eb16a71
ErrolNtetha <mphumier@gmail.com>
2021-09-22 20:08:42 +0200

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Header from './components/Header';
 import Home from './components/Home';
+import Messages from './components/Messages'
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-        <div className="App">
+        <Router>
+          <div className="App">
           <Header />
-          <Home />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/messages' component={Messages} />
+          </Switch>
         </div>
+        </Router>
   );
 }
 
