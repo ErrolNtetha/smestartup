fff130af3fe00aa02fc105553372b93ae6c5d56e
ErrolNtetha <mphumier@gmail.com>
2021-10-06 20:40:34 +0200

    added a new property

--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@ export const companyBrief = [
         investmentAmount: 20000,
         typeOfCompany: '(Pty) Ltd.',
         numOfEmployees: '200-350',
+        summary: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem veritatis optio eveniet, suscipit tenetur eum a error itaque dolore architecto?",
         id: 0,
     }
 ]
