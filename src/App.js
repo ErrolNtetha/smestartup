2745e38ae031e9c5e843feb52265e3aa51bdb417
ErrolNtetha <mphumier@gmail.com>
2021-10-16 20:17:30 +0200

    added a new route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home';
 import Messages from './components/Messages'
 import {BrowserRouter as HashRouter, Switch, Route } from 'react-router-dom';
 import Network from './components/Pages/Network';
+import ChatWindow from './components/ChatWindow';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
             <Route path='/' exact component={() => <Home authorized={false} />} />
             <Route path='/messages' component={Messages} />
             <Route path='/mynetwork' component={Network} />
+            <Route path='/inbox/message' component={ChatWindow} />
           </Switch>
         </div>
         </HashRouter>
