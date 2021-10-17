8b56f236d912806612400c12d17b40a0c3afcccb
ErrolNtetha <mphumier@gmail.com>
2021-10-17 19:52:26 +0200

    added some styles

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,14 +1,20 @@
 import React from 'react'
+import '../styles/header.scss'
 
 export default function ChatWindow() {
     return (
-        <section className="searchContainer">
-            <div className="searchBar">
-                <section className="search">
-                   
-                </section>
-                <input type="text" placeholder='Type a message' />
-            </div>
-        </section>
+        <>
+            <section>
+                <p className='textMesssage'> Hello there! </p>
+            </section>
+            <section className="searchContainer">
+                <div className="searchBar">
+                    <section className="search">
+                    
+                    </section>
+                    <input type="text" placeholder='Type a message' />
+                </div>
+            </section>
+        </>
     )
 }
