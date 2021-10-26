3a607cc78455dc399b847b6adaf3314d155446c6
ErrolNtetha <mphumier@gmail.com>
2021-10-26 18:52:08 +0200

    created a new folder for redux store

---
+++ src/store/actions/dispatch.js
@@ -0,0 +1,3 @@
+import store from '../index';
+
+store.dispatch();
---
+++ src/store/reducers/reducer.js
@@ -0,0 +1,13 @@
+// Define the initial state here
+// and pass the through the state in the reducer
+
+const InitialState = {
+    counter: 0,
+    title: 'Learning Redux',
+}
+
+const reducer = (state = InitialState, action) => {
+    return state;
+}
+
+export default reducer;
