d53b615d5fab58ffd5e855cf7b44e2064e3761be
ErrolNtetha <mphumier@gmail.com>
2021-12-08 11:43:04 +0200

    added a check on the database for user

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,21 +1,22 @@
 const User = require('../models/user.model');
 
-const validateLogin = (req, res) => {
+const validateLogin = async (req, res) => {
     const user_email = req.body.email;
     const user_password = req.body.password;
 
     // Check on the database if we have the details 
     // the user entered on login
-    User.findOne({ email: user_email }, function(err, data) {
-
-        if(!err) {
-            if(data.email === user_email) {
-                console.log('Email already exist. Please login instead.');
+    await User.findOne({ email: user_email })
+        .then(user => {
+            if(!user) {
+                console.log('Invalid email or password. ');
+                console.log(user);
             }
-            else console.log('Email or password incorrect.');
-        }
-        else console.log(err);
-    })
+            else {
+                console.log('User granted entry.');
+                console.log(user.password);
+            }
+        })
 }
 
 module.exports = validateLogin;
