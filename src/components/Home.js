e3c207c4d2caad121890e8d30e7470c7392350f2
ErrolNtetha <mphumier@gmail.com>
2022-02-01 01:06:58 +0200

    Removed unsused imports

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,9 @@
 import React from "react";
 import Post from "./Post";
-import { Redirect } from 'react-router-dom';
 import CreatePost from "./CreatePost";
-import { BeatLoader } from 'react-spinners';
-import ToLet from './ToLet';
-import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
 
 export default function Home() {
-  const counter = useSelector(state => state.counter);
-  const dispatch = useDispatch();
-
   return (
     <section>
       <section>
