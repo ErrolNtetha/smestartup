38645b731e920d0ee7ea2f7660d94be54f55d088
ErrolNtetha <mphumier@gmail.com>
2022-02-01 00:58:03 +0200

    Changed the background color
    Fixed the columns on the deskot view to be straight rather than unven. Also changed the background color to pure white.

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Post from './Post';
+import Header from './Header';
 
 
 export default function Feed() {
@@ -7,16 +8,51 @@ export default function Feed() {
     const posts = ['post 1', 'post 2', 'post 1', 'post 2', 'post 1', 'post 2'];
 
     return (
-        <section className='feedContainer'>
+       <>
+         <Header headerContainer='headerContainer' style={{ color: '#fff' }} />
+         <section className='feedContainer'>
             <aside>
-                this is the left content
+                <section>
+                    <h3> Profile </h3>
+                    
+                    <section>
+                        
+                    </section>
+                </section>
             </aside>   
             <main>
                 {posts.map(post => <Post post={post} /> )}
             </main>
             <aside>
-                this is the right content
+                <section>
+                    <h3> Investors </h3>
+                    {/* <hr className='divider' /> */}
+                    <ul>
+                        <hr className='divider' />
+                        <li> 
+                            <span> Njabulo Ndlovu </span> 
+                            <button> Follow </button> 
+                        </li>
+                        <hr className='divider' />
+                        <li> 
+                            <span> Syanda Dlamini </span> 
+                            <button> Follow </button> 
+                        </li>
+                        <hr className='divider' />
+                        <li> 
+                            <span> Thabani Makhanya </span> 
+                            <button> Follow </button> 
+                        </li>
+                        <hr className='divider' />
+                        <li> 
+                            <span> Mphumeleli Ntetha </span> 
+                            <button> Follow </button> 
+                        </li>
+                    </ul>
+                </section>
+                
             </aside>
         </section>
+       </>
     )
 }
