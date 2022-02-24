a188c874dd7b9583a1618694c76ea674c9cdabea
ErrolNtetha <mphumier@gmail.com>
2022-02-24 21:44:28 +0200

    Links for the navigations in header components.

---
+++ src/views/header/utils.tsx
@@ -0,0 +1,30 @@
+export const nav = [
+    {
+		name: 'Home',
+		id: 'home-link',
+		url: '/home/',
+		isPrivate: false,
+		key: 0,
+	},
+	{
+		name: 'About',
+		id: 'home-link',
+		url: '/about/',
+		isPrivate: false,
+		key: 1,
+	},
+	{
+		name: 'Contact',
+		id: 'contact-link',
+		url: '/contact/',
+		isPrivate: false,
+		key: 2,
+	},
+	{
+		name: 'Profile',
+		id: 'profile-link',
+		url: '/profile/',
+		isPrivate: true,
+		key: 3,
+	},
+];
