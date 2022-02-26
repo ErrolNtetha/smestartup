569203c5bffbacb23987b7398bfefe653ff0df31
ErrolNtetha <mphumier@gmail.com>
2022-02-26 20:53:48 +0200

    Added a browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 /* eslint-disable func-names */
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Main } from './routes';
+import './scss/_index.scss';
 
 const App:React.FC = function () {
   return (
+    <Router>
+      <div className='darkTheme'>
         <Main />
+      </div>
+    </Router>
   );
 };
 
