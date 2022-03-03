f64cd7f0467233883c72c9c740efde4072339ae1
ErrolNtetha <mphumier@gmail.com>
2022-03-03 21:48:15 +0200

    Minor changes

--- src/containers/register/index.tsx
+++ src/containers/register/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { useFormik } from 'formik';
+import axios from 'axios';
 import { Header } from '../../views/header';
+import { Button } from '../../components/button';
+// import { BASE_URL } from '../../config/baseURL';
 
 export const Register = () => {
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            email: '',
+            password: ''
+        },
+        onSubmit(values) {
+            if (!values.firstName || !values.password) {
+                console.log('Fields empty...');
+                return;
+            }
+            axios.post('http://localhost:5000/signup', values)
+            .then((res) => console.log(res))
+            .catch((err) => console.log(err));
+        }
+    });
+
     return (
         <>
         <Header />
@@ -11,7 +32,44 @@ export const Register = () => {
                         Grow your business at the comfort of your own bed!
                     </section>
                     <section className='register__right'>
-                        this is right
+                       <span className='register__header'>  Create New Account </span>
+                        <form onSubmit={formik.handleSubmit}>
+                        <label className='login__label' htmlFor='firstName'> Name
+                            <input
+                              type='text'
+                              name='firstName'
+                              placeholder='Mphumeleli Ntetha'
+                              value={formik.values.firstName}
+                              onChange={formik.handleChange}
+                              className='login__emailField'
+                              autoCapitalize='false'
+                            />
+                        </label>
+                        <label className='login__label' htmlFor='email'> Name
+                            <input
+                              type='email'
+                              name='email'
+                              placeholder='name@example.com'
+                              value={formik.values.email}
+                              onChange={formik.handleChange}
+                              className='login__emailField'
+                              autoCapitalize='false'
+                            />
+                        </label>
+
+                        <label className='login__label' htmlFor='email'> Password:
+                            <input
+                              type='password'
+                              name='password'
+                              placeholder='Enter password'
+                              value={formik.values.password}
+                              onChange={formik.handleChange}
+                              className='login__emailField'
+                              autoComplete='false'
+                            />
+                        </label>
+                            <Button type='submit' className='login__button'> Create Account </Button>
+                        </form>
                     </section>
                 </section>
             </section>
