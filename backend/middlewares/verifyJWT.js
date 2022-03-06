1b738f119e8bbdbce235efc540fe174dcaddc4da
ErrolNtetha <mphumier@gmail.com>
2022-03-07 00:00:29 +0200

    Fixed the fetch token

--- backend/middlewares/verifyJWT.js
+++ backend/middlewares/verifyJWT.js
@@ -1,31 +1,27 @@
 const jwt = require('jsonwebtoken');
 
 function verifyJWT(req, res, next) {
-    const token = req.headers["x-access-token"]; 
- 
-    if(token) {
+    const token = req.headers['x-access-token'].split(' ')[1];
+
+    if (token) {
         jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-            if(err) {
+            if (err) {
                 return res.json({
                     isLoggedIn: false,
                     message: "Aunthetication failed",
                 });
             }
  
-            // create an empty abject  
+            // create an empty abject
             req.user = {};
             req.user.id = decoded.id;
             req.user.email = decoded.email;
             console.log('Token validated ');
-
-            console.log(req.user.id);
-            console.log(req.user.email);
-            res.json({ isLoggedIn: true });
             next();
         });
     }
     else {
-        res.json({ message: 'Invalid token.', isLogged: false });
+        res.json({ message: 'Invalid token.', isLoggedIn: false });
         console.log('invalid token');
     }
 }
