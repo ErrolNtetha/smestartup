630c266186bd7cf6afe4f2105ea6ad406e0a488f
ErrolNtetha <mphumier@gmail.com>
2022-03-16 16:28:40 +0200

    Testing the date-fns with console.log

--- src/views/feed/lists.tsx
+++ src/views/feed/lists.tsx
@@ -3,34 +3,34 @@ import { PostField } from 'components/postField';
 import { List } from 'components/lists/list';
 import { RootState } from 'store';
 import { useSelector } from 'react-redux';
+import { formatDistance, subDays } from 'date-fns';
 import axios from 'axios';
 
 // these are the lists of the all posts
 // call the useEffect hook to fetch all posts from the database
 
 export const Lists = () => {
-const toggleState = useSelector((state: RootState) => state.isToggleOn);
-const [posts, setPosts] = React.useState([]);
+  const toggleState = useSelector((state: RootState) => state.isToggleOn);
+  const [posts, setPosts] = React.useState([]);
 
-React.useEffect(() => {
-  axios.get('/feed', {
-    headers: {
-      'x-access-token': localStorage.getItem('token')
-    }
-  })
-    .then((res) => {
-      setPosts(res.data.posts);
+  React.useEffect(() => {
+    console.log(formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true }));
+
+    axios.get('/feed', {
+      headers: {
+        'x-access-token': localStorage.getItem('token')
+      }
     })
-    .catch((err) => console.error(err));
-}, []);
+      .then((res) => {
+        setPosts(res.data.posts);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
-const today = new Date();
-console.log(today);
-
-    return (
-        <div>
-            { toggleState ? <PostField /> : null }
-            {posts.map((post) => <List post={post} name={post} key={post._id} />)}
-        </div>
-    );
+  return (
+    <div>
+      {toggleState ? <PostField /> : null}
+      {posts.map((post) => <List post={post} name={post} key={post._id} />)}
+    </div>
+  );
 };
