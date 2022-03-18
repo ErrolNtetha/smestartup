7dbc690dda48ebf2509aa8c0bbd69b0e49a68f9b
ErrolNtetha <mphumier@gmail.com>
2022-03-18 17:36:39 +0200

    Added a loader when retrieving posts from the database.

--- src/components/lists/list.tsx
+++ src/components/lists/list.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { FiStar, FiMoreHorizontal } from 'react-icons/fi';
 import avatar from 'assets/avatar.png';
 import { Button } from 'components/button';
+import { Modal } from 'components/modal';
 
 interface Props {
   name: {
@@ -11,6 +12,12 @@ interface Props {
 }
 
 export const List:FC<Props> = ({ name, post }) => {
+  const [modal, setModal] = React.useState(false);
+
+  const handleToggle = (): boolean => {
+    setModal(!modal);
+  };
+
   return (
     <section className='feed__list'>
       <span className='feed__firstRow'>
@@ -20,9 +27,10 @@ export const List:FC<Props> = ({ name, post }) => {
             <h4 className='feed__name'> {name.user} </h4>
             <p className='feed__title'> Founder and CEO, Blendot </p>
             <p className='feed__recent'> 32 mins ago </p>
+            {modal && <Modal handleToggle={handleToggle} />}
           </span>
         </div>
-        <FiMoreHorizontal className='feed__options' />
+        <FiMoreHorizontal className='feed__options' onClick={() => setModal(!modal)} />
       </span>
 
       <p className='feed__listContent'> {post.post} </p>
@@ -31,7 +39,7 @@ export const List:FC<Props> = ({ name, post }) => {
       <section className='feed__LastRow'>
         <span className='feed__stats'>
           <span className='feed__comments'>  </span>
-          <Button className='feed__stats__bookmarks'> <FiStar className='feed__starIcon' /> 329 </Button>
+          <Button className='feed__stats__bookmarks'> <FiStar className='feed__starIcon' /> 19 </Button>
         </span>
       </section>
     </section>
