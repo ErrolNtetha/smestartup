87b4693379802151a40260d40283a773e8025b86
ErrolNtetha <mphumier@gmail.com>
2022-03-18 17:35:41 +0200

    Created a modal when option icon on the top right corner is clicked

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { FiX } from 'react-icons/fi';
 
-export const Modal = () => {
+interface Props {
+  handleToggle: boolean,
+  id: string
+}
+
+export const Modal:React.FC<Props> = ({ handleToggle, id }) => {
   return (
-    <div>
-        This is a modal
+    <div className='feed__modalContainer'>
+        <section className='feed__modal'>
+           The id of this post is {id}
+
+          <section className='feed__modalClose'>
+            <FiX onClick={handleToggle} />
+          </section>
+        </section>
     </div>
   );
 };
