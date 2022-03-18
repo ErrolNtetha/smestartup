739dac5c1ac7b3fe754fd64f2d762f971ada4f91
ErrolNtetha <mphumier@gmail.com>
2022-03-18 13:14:09 +0200

    Added a loader in the field so that the user will see that data is loading. UX good practices.

--- src/views/feed/lists.tsx
+++ src/views/feed/lists.tsx
@@ -3,6 +3,8 @@ import { PostField } from 'components/postField';
 import { List } from 'components/lists/list';
 import { RootState } from 'store';
 import { useSelector } from 'react-redux';
+import { ScaleLoader } from 'react-spinners';
+import { useHistory } from 'react-router-dom';
 import { formatDistance, subDays } from 'date-fns';
 import axios from 'axios';
 
@@ -12,17 +14,26 @@ import axios from 'axios';
 export const Lists = () => {
   const toggleState = useSelector((state: RootState) => state.isToggleOn);
   const [posts, setPosts] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const history = useHistory();
 
   React.useEffect(() => {
     console.log(formatDistance(subDays(new Date(), 4), new Date(), { addSuffix: true }));
-
     axios.get('/feed', {
       headers: {
         'x-access-token': localStorage.getItem('token')
       }
     })
       .then((res) => {
+        if (res.data.isLoggedIn === 'false') {
+          // i want to check for the response and if returns false, redirect to login screen
+          history.push('/login');
+          return;
+        }
+
+        setLoading(false);
         setPosts(res.data.posts);
+        console.log(loading);
       })
       .catch((err) => console.error(err));
   }, []);
@@ -30,7 +41,7 @@ export const Lists = () => {
   return (
     <div>
       {toggleState ? <PostField /> : null}
-      {posts.map((post) => <List post={post} name={post} key={post._id} />)}
+      { loading ? <section className='feed__loader'> <ScaleLoader color='white' /> </section> : posts.map((post) => <List post={post} name={post} key={post._id} />)}
     </div>
   );
 };
