9772b762174980791de01ba52dc0f020fd3369a0
ErrolNtetha <mphumier@gmail.com>
2022-03-24 20:01:30 +0200

    Added a an radio button. Not yet working.

--- src/containers/register/index.tsx
+++ src/containers/register/index.tsx
@@ -16,10 +16,12 @@ export const Register = () => {
             email: '',
             password: '',
             file: '',
+            gender: ''
         },
         onSubmit(values) {
             if (!values.firstName || !values.email || !values.password) {
                 console.log('Fields empty...');
+                console.log(values);
                 return;
             }
 
@@ -41,71 +43,90 @@ export const Register = () => {
         <>
         <Header />
             <section className='register'>
+                <p className='register__introHeader'> Signup up for a free account! </p>
                 <section className='register__container'>
                     <section className='register__left'>
                         Grow your business at the comfort of your own bed!
                     </section>
                     <section className='register__right'>
-                       <span className='register__header'>  Create New Account </span>
+                       <span className='register__header'>  Personal Details </span>
                         <form onSubmit={formik.handleSubmit}>
-                        <label className='login__label' htmlFor='firstName'> First Name
+                        <label className='register__label' htmlFor='firstName'> First Name
                             <input
                               type='text'
                               name='firstName'
                               placeholder='Mphumeleli Ntetha'
                               value={formik.values.firstName}
                               onChange={formik.handleChange}
-                              className='login__emailField'
+                              className='register__emailField'
                               autoCapitalize='false'
                             />
                         </label>
-                        <label className='login__label' htmlFor='firstName'> Last Name
+                        <label className='register__label' htmlFor='firstName'> Last Name
                             <input
                               type='text'
                               name='lastName'
                               placeholder='Ntetha'
                               value={formik.values.lastName}
                               onChange={formik.handleChange}
-                              className='login__emailField'
+                              className='register__emailField'
                               autoCapitalize='false'
                             />
                         </label>
-                        <label className='login__label' htmlFor='email'> Email
+                        <label className='register__label' htmlFor='email'> Email
                             <input
                               type='email'
                               name='email'
                               placeholder='name@example.com'
                               value={formik.values.email}
                               onChange={formik.handleChange}
-                              className='login__emailField'
+                              className='register__emailField'
                               autoCapitalize='false'
                             />
                         </label>
 
-                        <label className='login__label' htmlFor='email'> Password:
+                        <label className='register__label' htmlFor='gender'> Gender
+                            <input
+                              type='radio'
+                              name='male'
+                              value={formik.values.gender}
+                              onChange={formik.handleChange}
+                              className='register__emailField'
+                            />
+                            <input
+                              type='radio'
+                              name='female'
+                              value={formik.values.gender}
+                              onChange={formik.handleChange}
+                              className='register__emailField'
+                            />
+                        </label>
+
+                        <label className='register__label' htmlFor='email'> Password:
                             <input
                               type='password'
                               name='password'
                               placeholder='Enter password'
                               value={formik.values.password}
                               onChange={formik.handleChange}
-                              className='login__emailField'
+                              className='register__emailField'
                               autoComplete='false'
                             />
                         </label>
-                        <label className='login__label' htmlFor='email'> Password:
+                        {/* <label className='register__label' htmlFor='email'> Password:
                             <Button className='login__button'>
                                 Choose Avatar
                             <input
                               type='file'
+                            />
                               name='avatar'
                               accept='image/*'
                               onChange={(event) => formik.setFieldValue('file', event.currentTarget.files[0])}
-                              className='login__emailField'
+                              className='register__emailField'
                             />
                             </Button>
-                        </label>
-                            <Button type='submit' className='login__button'> Create Account </Button>
+                        </label> */}
+                            <Button type='submit' className='register__button'> Create Account </Button>
                         </form>
                     </section>
                 </section>
