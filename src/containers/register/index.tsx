6568e364253e7da7df71d3b16a45b881ff0053ea
ErrolNtetha <mphumier@gmail.com>
2022-03-27 21:13:10 +0200

    Created a global state to hold form values

--- src/containers/register/index.tsx
+++ src/containers/register/index.tsx
@@ -1,136 +1,48 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
 import { useFormik } from 'formik';
-import axios from 'axios';
-import { useHistory } from 'react-router-dom';
+// import axios from 'axios';
+// import { useHistory } from 'react-router-dom';
 import { Header } from '../../views/header';
+import { User } from './user-0';
+import { InitialForm } from './initialForm';
+import { Business } from './business';
 import { Button } from '../../components/button';
-// import { BASE_URL } from '../../config/baseURL';
+// import { SERVER_URL } from '../../config/baseURL';
 
 export const Register = () => {
-    const history = useHistory();
+    const [pages, setPages] = React.useState(0);
 
     const formik = useFormik({
-        initialValues: {
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: '',
-            file: '',
-            gender: ''
-        },
+        initialValues: {},
         onSubmit(values) {
-            if (!values.firstName || !values.email || !values.password) {
-                console.log('Fields empty...');
-                console.log(values);
-                return;
-            }
-
-            // console.log(values);
-            // console.log(values.file);
-
-            axios.post('http://localhost:5000/signup', values)
-            .then((res) => {
-                if (res) {
-                    console.log(values);
-                    history.push('/login');
-                }
-            })
-            .catch((err) => console.log(err));
+           console.log(values);
+           handleNext();
         }
     });
 
+    const handlePrev = () => {
+        setPages((prevState) => prevState - 1);
+    };
+
+    const handleNext = () => {
+        setPages(pages + 1);
+        console.log('Test: ', formik.values);
+    };
+
     return (
         <>
-        <Header />
-            <section className='register'>
-                <p className='register__introHeader'> Signup up for a free account! </p>
-                <section className='register__container'>
-                    <section className='register__left'>
-                        Grow your business at the comfort of your own bed!
-                    </section>
-                    <section className='register__right'>
-                       <span className='register__header'>  Personal Details </span>
-                        <form onSubmit={formik.handleSubmit}>
-                        <label className='register__label' htmlFor='firstName'> First Name
-                            <input
-                              type='text'
-                              name='firstName'
-                              placeholder='Mphumeleli Ntetha'
-                              value={formik.values.firstName}
-                              onChange={formik.handleChange}
-                              className='register__emailField'
-                              autoCapitalize='false'
-                            />
-                        </label>
-                        <label className='register__label' htmlFor='firstName'> Last Name
-                            <input
-                              type='text'
-                              name='lastName'
-                              placeholder='Ntetha'
-                              value={formik.values.lastName}
-                              onChange={formik.handleChange}
-                              className='register__emailField'
-                              autoCapitalize='false'
-                            />
-                        </label>
-                        <label className='register__label' htmlFor='email'> Email
-                            <input
-                              type='email'
-                              name='email'
-                              placeholder='name@example.com'
-                              value={formik.values.email}
-                              onChange={formik.handleChange}
-                              className='register__emailField'
-                              autoCapitalize='false'
-                            />
-                        </label>
+            <Header />
+            { pages === 0
+            ? <InitialForm click={handleNext} type='type' submit={formik.handleSubmit} setType={formik.handleChange} />
+            : pages === 1
+            ? <User />
+            : pages === 2
+            ? <Business />
+            : 'You have reached the dead end' }
 
-                        <label className='register__label' htmlFor='gender'> Gender
-                            <input
-                              type='radio'
-                              name='male'
-                              value={formik.values.gender}
-                              onChange={formik.handleChange}
-                              className='register__emailField'
-                            />
-                            <input
-                              type='radio'
-                              name='female'
-                              value={formik.values.gender}
-                              onChange={formik.handleChange}
-                              className='register__emailField'
-                            />
-                        </label>
-
-                        <label className='register__label' htmlFor='email'> Password:
-                            <input
-                              type='password'
-                              name='password'
-                              placeholder='Enter password'
-                              value={formik.values.password}
-                              onChange={formik.handleChange}
-                              className='register__emailField'
-                              autoComplete='false'
-                            />
-                        </label>
-                        {/* <label className='register__label' htmlFor='email'> Password:
-                            <Button className='login__button'>
-                                Choose Avatar
-                            <input
-                              type='file'
-                            />
-                              name='avatar'
-                              accept='image/*'
-                              onChange={(event) => formik.setFieldValue('file', event.currentTarget.files[0])}
-                              className='register__emailField'
-                            />
-                            </Button>
-                        </label> */}
-                            <Button type='submit' className='register__button'> Create Account </Button>
-                        </form>
-                    </section>
-                </section>
-            </section>
+            <Button onClick={handlePrev}> Previous </Button>
+            <Button onClick={handleNext}> Next </Button>
         </>
     );
 };
