e586ad0f28459166ca2880992e0bd9b06609ebf7
ErrolNtetha <mphumier@gmail.com>
2022-04-04 21:34:30 +0200

    Added a route for delete request

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const app = require('express');
+
 const router = express.Router();
 
 // import routes
-const postRoutes = require('../controllers/postsController'); 
+const postRoutes = require('../controllers/postsController');
 const verifyToken = require('../middlewares/verifyJWT');
+const deletePost = require('../controllers/deletePostController');
 
 // Handle routes
 router.get('/feed', verifyToken, postRoutes.getUserPost);
 router.post('/feed', verifyToken, postRoutes.userPosts);
- 
+router.delete('/feed/:id', verifyToken, deletePost);
+
 module.exports = router;
- 
