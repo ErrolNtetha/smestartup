d252b5fb55408cca64ed8001d24012d79ce3c213
ErrolNtetha <mphumier@gmail.com>
2022-04-06 11:51:32 +0200

    Fixed the error on Functions are not valid as a React child.

--- src/containers/register/business.tsx
+++ src/containers/register/business.tsx
@@ -9,7 +9,7 @@ import {
     FormikValues
 } from 'formik';
 import { Input } from 'components/input';
-import { Button } from '../../components/button';
+// import { Button } from '../../components/button';
 // import { SERVER_URL } from '../../config/baseURL';
 
 interface Props {
@@ -17,66 +17,43 @@ interface Props {
         firstName: string;
         lastName: string;
     } | null;
-    onSubmit: React.FormEventHandler<HTMLFormElement>;
+    // onSubmit: React.FormEventHandler<HTMLFormElement>;
     // nextPage: React.MouseEventHandler<HTMLButtonElement>;
-    prevPage: React.MouseEventHandler<HTMLButtonElement>;
+    //    prevPage: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-export const Business = ({
-prevPage, onSubmit, value
-}: Props) => {
+export const Business = ({ value }: Props) => {
     return (
-            <section className='register'>
-                <section className='register__container'>
-                    <section className='register__left'>
-                        Grow your business at the comfort of your own bed!
-                    </section>
-                    <Stepper
-                      initialValues={{
-                         firstName: '',
-                         lastName: '',
-                        }}
-                      onSubmit={(values) => {
-                          console.log(values);
-                      }}
-                    >
-                       {(props) => (
-                            <section className='register__right'>
-                            <span className='register__header'>  Company Information </span>
-                                <Form onSubmit={props.handleSubmit}>
-                                    <div>
-                                    <Field
-                                      label='first name'
-                                      name='firstName'
-                                      type='text'
-                                      value={value.firstName}
-                                      component={Input}
-                                      placeholder='Enter your name'
-                                      className='register__emailField'
-                                      onChange={props.handleChange}
-                                    />
-                                    </div>
-
-                                    <div>
-                                    <Field
-                                      name='lastName'
-                                      type='text'
-                                      value={value.lastName}
-                                      component={Input}
-                                      placeholder='Enter your last name'
-                                      className='register__emailField'
-                                    />
-                                    </div>
-                                    <span className='register__btnContainer'>
-                                        <Button type='submit' onClick={prevPage} className='register__button'> Back </Button>
-                                        <Button type='submit' onClick={onSubmit} className='register__button'> Next </Button>
-                                    </span>
-                                </Form>
-                            </section>
-                       )}
-                    </Stepper>
-                </section>
-            </section>
+        <Stepper
+          initialValues={{
+             firstName: '',
+             lastName: ''
+            }}
+          onSubmit={(values) => {
+                console.log(values);
+            }}
+        >
+            <>
+                <Field
+                  label='first name'
+                  name='firstName'
+                  type='text'
+                  value={value.firstName}
+                  component={Input}
+                  placeholder='Enter your first name'
+                  className='register__emailField'
+                />
+                <Field
+                  label='last name'
+                  name='lastName'
+                  type='text'
+                  value={value.lastName}
+                  component={Input}
+                  placeholder='Enter your last name'
+                  className='register__emailField'
+                />
+            </>
+        </Stepper>
     );
 };
 
