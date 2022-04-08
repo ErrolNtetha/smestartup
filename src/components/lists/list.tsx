9f8eb4b125e1b10b81093b1acf03c636bc73e11d
ErrolNtetha <mphumier@gmail.com>
2022-04-08 23:27:24 +0200

    Added a verified barge for verified accounts

--- src/components/lists/list.tsx
+++ src/components/lists/list.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable operator-linebreak */
 import React, { FC } from 'react';
 import { FiStar, FiMoreHorizontal } from 'react-icons/fi';
+import { MdVerified } from 'react-icons/md';
 import avatar from 'assets/avatar.png';
 import { Button } from 'components/button';
 import { Modal } from 'components/modal';
 import { formatDistance } from 'date-fns';
+// import { NODE_ENV } from 'config/baseURL';
 
 interface Props {
   name: {
@@ -19,22 +22,28 @@ export const List:FC<Props> = ({
 }) => {
   const [modal, setModal] = React.useState(false);
 
-  const handleToggle = () => {
-    setModal(!modal);
-  };
+    // const handleToggle = () => {
+    // setModal(!modal);
+    // };
 
-  return (
+return (
     <section className='feed__list'>
       <span className='feed__firstRow'>
         <div className='feed__profile'>
           <img src={avatar} alt='me' className='feed__profileImage' />
           <span>
-            <h4 className='feed__name'> {name.user} </h4>
+              <h4 className='feed__name'> {name.user} <MdVerified /> </h4>
             <p className='feed__title'> Works at Company  </p>
             <p className='feed__recent'> {formatDistance(new Date(date), new Date(), { addSuffix: true })} </p>
           </span>
         </div>
-        {modal && <Modal handleToggle={handleToggle} id={id} />}
+        {modal &&
+            (
+                <Modal>
+                    <section> The ID of this post is: {id} </section>
+                    <Button className='feed__modal--delete'> Delete </Button>
+                </Modal>
+            )}
         <FiMoreHorizontal className='feed__options' onClick={() => setModal(!modal)} />
       </span>
 
