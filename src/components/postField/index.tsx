8f52a6714c3264627f85b86d57c3dc953725e76a
ErrolNtetha <mphumier@gmail.com>
2022-04-08 23:26:14 +0200

    Added an image preview when the user selects an image from their computer or mobile phone

--- src/components/postField/index.tsx
+++ src/components/postField/index.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable no-unused-vars */
 import { Button } from 'components/button';
-import React, { useState, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { FiImage, FiVideo } from 'react-icons/fi';
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
@@ -8,7 +9,8 @@ import toggleFieldOff from 'store/actions/toggleField_OFF';
 export const PostField = () => {
 const dispatch = useDispatch();
 const [post, setPost] = useState('');
-const [images, setImages] = useState<string[]>([]);
+const [images, setImages] = useState<File>();
+const [fileURL, setFileURL] = useState<string>();
 // const [videos, setVideos] = useState(null);
 const [loading, setLoading] = useState(false);
 const imageInput = useRef(null);
@@ -45,33 +47,53 @@ const handleSubmit = async () => {
   .catch((err) => console.error(err));
 };
 
+useEffect(() => {
+    if (images) {
+        const reader = new FileReader();
+
+        reader.onload = () => {
+            setFileURL(reader.result as string);
+        };
+        reader.readAsDataURL(images);
+    }
+}, [images]);
+console.log(fileURL);
 // console.log(images[2]);
 
   return (
     <section className='feed__postField'>
-        <textarea name='post' className='feed__textarea' rows={6} placeholder='Share what is happening today...' onChange={(e) => setPost(e.target.value)} />
-
+        <section>
+        <textarea
+          name='post'
+          className='feed__textarea'
+          rows={6}
+          placeholder='What is happening?'
+          onChange={(e) => setPost(e.target.value)}
+        />
+        <section>
+            <img src={fileURL} alt='screenshot' className='feed__imagePreview' style={{ width: '80px' }} />
+        </section>
         <section className='feed__btnGroup'>
           <section className='feed__left'>
-            <FiImage className='feed__image' onClick={() => imageInput.current.click()} />
+              <FiImage className='feed__image' onClick={() => imageInput.current.click()} />
             <input
               ref={imageInput}
               hidden
               accept='image/*'
               multiple
-              onChange={(e) => setImages(e.target.files)}
+              onChange={(e) => {
+                setImages(e.target.files[0]);
+              }}
               type='file'
             />
-            <section>
-              {images.map((image) => console.log(image.name))}
-            </section>
 
             <FiVideo className='feed__video' />
           </section>
           <section className='feed__right'>
             <Button onClick={() => dispatch(toggleFieldOff())} className='feed__btn--cancel'> Cancel </Button>
-            <Button onClick={handleSubmit} className='feed__btn--post'> {loading ? 'loading' : 'Post'} </Button>
+            <Button onClick={handleSubmit} className='feed__btn--post'> {loading === null ? 'loading' : 'Post'} </Button>
           </section>
+        </section>
         </section>
     </section>
   );
