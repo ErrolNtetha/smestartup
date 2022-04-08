32c3a67f190fa53dd1ebc8997157bdb2a4854e2f
ErrolNtetha <mphumier@gmail.com>
2022-04-08 16:33:13 +0200

    Fix the Modal

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,24 +1,17 @@
-import { Button } from 'components/button';
 import React from 'react';
 import { FiX } from 'react-icons/fi';
 
 interface Props {
-  handleToggle: boolean,
-  id: string
+  children: React.ReactNode;
 }
 
-export const Modal:React.FC<Props> = ({ handleToggle, id }) => {
-  return (
-    <div className='feed__modalContainer'>
-        <section className='feed__modal'>
-          <p> The ID of this post is {id} </p>
-
-          <Button className='feed__modal--delete' onClick={() => console.log('delete clicked')}> Delete </Button>
-
-          <section className='feed__modalClose'>
-            <FiX onClick={handleToggle} />
-          </section>
-        </section>
-    </div>
-  );
+export const Modal:React.FC<Props> = ({ children }) => {
+    return (
+        <div className='feed__modalContainer'>
+            <section className='feed__modal'>
+                {children}
+                <section className='feed__modalClose'> <FiX /> </section>
+            </section>
+        </div>
+    );
 };
