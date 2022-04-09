e425f33cd3b16f97acd002c5165885d5e65d7877
ErrolNtetha <mphumier@gmail.com>
2022-04-09 12:03:48 +0200

    Fixed the delete controller. Now works fine.

--- backend/controllers/deletePostController.js
+++ backend/controllers/deletePostController.js
@@ -3,13 +3,13 @@ const Post = require('../models/post.model');
 const deletePost = async (req, res) => {
     const id = req.params.id;
 
-    await Post.findOneAndRemove(id).exec()
-        .then(res => {
-            if (!res) {
+    await Post.findOneAndRemove({ _id: id }).exec()
+        .then(post => {
+            if (!post) {
                 console.log('No post found for that ID');
                 return;
             }
-            return console.log({ message: 'Post successfully deleted!' });
+            return res.json(post);
         })
         .catch(err => console.log(err));
 }
