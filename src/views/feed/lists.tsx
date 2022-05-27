3df975158caf600cbe3fabeaabe5d79e3fb6ff87
ErrolNtetha <mphumier@gmail.com>
2022-05-27 11:21:06 +0200

    Sorted the posts by latest data and time

--- src/views/feed/lists.tsx
+++ src/views/feed/lists.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { PostField } from 'components/postField';
 import { List } from 'components/lists/list';
 import { RootState } from 'store';
+// import { useFetchData } from 'hooks/useFetchData';
 import { useSelector } from 'react-redux';
 import { ScaleLoader } from 'react-spinners';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
+// import { SERVER_URL } from 'config/baseURL';
 import axios from 'axios';
 
 // these are the lists of all posts
@@ -14,26 +16,24 @@ export const Lists = () => {
   const toggleState = useSelector((state: RootState) => state.isToggleOn);
   const [posts, setPosts] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
-  const history = useHistory();
+
+    //    const feed = useFetchData(`${SERVER_URL}/feed`);
 
   React.useEffect(() => {
-    axios.get('https://backend-sme.herokuapp.com/feed', {
+    const fetchPosts = async () => {
+        await axios.get('https://backend-sme.herokuapp.com/feed', {
       headers: {
         'x-access-token': localStorage.getItem('token')
       }
     })
       .then((res) => {
-        if (res.data.isLoggedIn === 'false') {
-          // i want to check for the response and if returns false, redirect to login screen
-          history.push('/login');
-          return;
-        }
-
         setLoading(false);
-        setPosts(res.data.posts);
-        console.log(res.data.posts);
+        setPosts(res.data.posts.sort((a: string, b: string) => a.createdAt < b.createdAt));
+          console.log(res.data.posts);
       })
       .catch((err) => console.error(err));
+      };
+      return fetchPosts();
   }, []);
 
   return (
@@ -41,7 +41,18 @@ export const Lists = () => {
       <section className='feed__postFieldContainer'>
         {toggleState ? <PostField /> : null}
       </section>
-      { loading ? <section className='feed__loader'> <ScaleLoader color='white' /> </section> : posts.map((post) => <List post={post} name={post} key={post._id} date={post.createdAt} id={post._id} />)}
+      { loading ? <section className='feed__loader'> <ScaleLoader color='white' /> </section>
+          : posts.map((post) => (
+              <List
+                post={post}
+                image={post}
+                isVerified={post}
+                name={post}
+                key={post._id}
+                date={post}
+                id={post}
+              />
+              ))}
     </div>
   );
 };
