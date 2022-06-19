ab790dc4ee0111c31c1b99259fe9ed74efd895d6
ErrolNtetha <mphumier@gmail.com>
2022-06-19 19:58:32 +0200

    Refactor: Used custom hook and refactored the component

--- src/views/feed/lists.tsx
+++ src/views/feed/lists.tsx
@@ -1,52 +1,35 @@
 import React from 'react';
 import { PostField } from 'components/postField';
-import { axiosPrivate } from 'config/axiosInstance';
 import { List } from 'components/lists/list';
 import { RootState } from 'store';
 import { Create } from 'components/create';
 // import { useFetchData } from 'hooks/useFetchData';
 import { useSelector } from 'react-redux';
 import { ScaleLoader } from 'react-spinners';
-// import { SERVER_URL } from 'config/baseURL';
-// import { useHistory } from 'react-router-dom';
-// import { axiosInstance } from 'config/axiosInstance';
-
-// these are the lists of all posts
-// call the useEffect hook to fetch all posts from the database
+import { useFetchData } from 'hoc/useFetchData';
 
 export const Lists = () => {
   const toggleState = useSelector((state: RootState) => state.isToggleOn);
-  const [posts, setPosts] = React.useState([]);
-  const [loading, setLoading] = React.useState(true);
+    const response = useFetchData('/feed');
 
-  React.useEffect(() => {
-    const fetchPosts = async () => {
-        await axiosPrivate.get('/feed')
-      .then((res) => {
-        setLoading(false);
-          setPosts(res.data.posts.sort((a: string, b: string) => b.createdAt > a.createdAt));
-          console.log(res.data.posts);
-      })
-      .catch((err) => console.error(err));
-      };
-      return fetchPosts();
-  }, []);
+    const { posts } = response.data;
+    console.log(response.data.posts);
 
-  return (
+ return (
     <div className='feed__feedWrapper'>
         {toggleState ? <PostField /> : null}
-        { loading ? <section className='feed__loader'> <ScaleLoader color='white' /> </section>
-          : posts.map((post) => (
+        { response.loading ? <section className='feed__loader'> <ScaleLoader color='white' /> </section>
+          : posts?.map((item) => (
               <List
-                post={post}
-                image={post}
-                isVerified={post}
-                name={post}
-                key={post._id}
-                date={post}
-                id={post}
-                title={post}
-                author={post}
+                post={item.post}
+                image={item.encodedImage}
+                isVerified={item.user.isVerified}
+                name={item.user.name}
+                key={item._id}
+                date={item.createdAt}
+                id={item._id}
+                occupation={item.user.occupation}
+                author={item.user._id}
               />
               ))}
         {!toggleState
