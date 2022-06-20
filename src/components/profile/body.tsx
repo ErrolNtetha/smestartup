d938a09660241499d6de2e351c29b072024aa6d3
ErrolNtetha <mphumier@gmail.com>
2022-06-20 19:13:46 +0200

    Add: Imported Post component

--- src/components/profile/body.tsx
+++ src/components/profile/body.tsx
@@ -1,14 +1,29 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
-// import { useFetchData } from '../../hooks/useFetchData';
-import { Post } from './post';
+import { UserPosts } from './post';
+import { useFetchPosts } from '../../hoc/useFetchPosts';
+// import { Tabs } from './tabs';
 
 export const Body = () => {
-    // const posts = useFetchData(`${NODE_ENV()}/posts`);
-    //  console.log(posts);
+    const { posts, error } = useFetchPosts();
+    console.log(posts);
+    console.log(error);
 
     return (
         <section className='profile__bodyContainer'>
-            <Post />
+            {
+                !posts.length
+                ? 'loading'
+                : posts.map(({ post, createdAt, _id }) => (
+                    <UserPosts
+                      post={post}
+                      date={createdAt}
+                      id={_id}
+                    />
+                    ))
+                ? error === 'ECONNABORTED'
+                : 'There was a problem with your connection.'
+            }
         </section>
     );
 };
