fe165239872802bbb7a49372cb6507bfcf4c2f61
ErrolNtetha <mphumier@gmail.com>
2022-06-26 14:30:13 +0200

    Add: Success proterty was added when delete request is fulfilled

--- backend/controllers/deletePostController.js
+++ backend/controllers/deletePostController.js
@@ -1,17 +1,21 @@
 const Post = require('../models/post.model');
 
 const deletePost = async (req, res) => {
-    const id = req.params.id;
+    const { id } = req.params;
 
     await Post.findOneAndRemove({ _id: id }).exec()
-        .then(post => {
+        .then((post) => {
             if (!post) {
                 console.log('No post found for that ID');
+                res.status(404).json({ success: false, message: 'Post was not found.' });
                 return;
             }
-            return res.json(post);
+            res.json({ success: true, message: 'Post successfully deleted' });
         })
-        .catch(err => console.log(err));
-}
+        .catch((error) => {
+            console.log(error.message);
+            res.status(500).json({ success: false, message: 'An error occured deleting your post.' });
+        });
+};
 
 module.exports = deletePost;
