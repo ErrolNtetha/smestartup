b989502ed525b9570bea8256086e5ae50907d06c
ErrolNtetha <mphumier@gmail.com>
2022-06-27 11:51:38 +0200

    Fix: Changed the text from lorem

--- src/views/hero/index.tsx
+++ src/views/hero/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { FiArrowRight } from 'react-icons/fi';
 import { Button } from '../../components/button';
 import { para, title } from './typo';
 
 export const Hero: React.FC = () => {
-  const history = useHistory();
+    const history = useHistory();
+    const isLoggedIn = useSelector((state) => state.isLogged);
+
   return (
     <section className='hero'>
         <aside className='hero__left'>
             <p className='hero__left__title'> {title} </p>
             <p className='hero__left__paragraph'> {para} </p>
-            <Button onClick={() => history.push('/register')} className='hero__left--action'> get started <FiArrowRight style={{ fontSize: '1.2rem' }} /> </Button>
+            { !isLoggedIn ? <Button onClick={() => history.push('/register')} className='hero__left--action'> get started <FiArrowRight style={{ fontSize: '1.2rem' }} /> </Button> : <Button onClick={() => history.push('/register')} className='hero__left--action'> go to feed <FiArrowRight style={{ fontSize: '1.2rem' }} /> </Button>}
         </aside>
         <aside className='hero__right'>
             This is left hero section
