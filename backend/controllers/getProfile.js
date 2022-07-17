4a59763b4428704fda5216c35da54bd611cdf3d6
ErrolNtetha <mphumier@gmail.com>
2022-07-17 19:46:55 +0200

    Add: Controller for get, put and delete request

--- backend/controllers/getProfile.js
+++ backend/controllers/getProfile.js
@@ -1,6 +1,6 @@
 const User = require('../models/user.model');
 
-exports.getUserProfile = async (req, res) => {
+exports.getProfile = async (req, res) => {
     const { email } = req.user;
 
     await User.findOne({ email })
@@ -8,4 +8,24 @@ exports.getUserProfile = async (req, res) => {
             res.json(result);
         })
         .catch((err) => console.error('An error occured while fetching profile data.', err));
+};
+
+exports.deleteProfile = async (req, res) => {
+    const { id } = req.params;
+
+    await User.findByIdAndRemove({ _id: id })
+        .then(() => {
+            res.status(200).json({ success: true, message: 'Account delete successfully.' });
+        })
+        .catch((error) => res.status(500).json({ success: false, error: error.message }));
+};
+
+exports.updateProfile = async (req, res) => {
+    const { id } = req.params;
+
+    await User.findByIdAndUpdate({ _id: id }, req.body)
+        .then(() => {
+            User.findOne({ _id: id }).then((user) => res.status(200).json({ user }));
+        })
+        .catch((error) => res.status(500).json({ success: false, error: error.message }));
 };
