c6182cbf10ba97622f4f57d8aa4ea0acb4d08094
ErrolNtetha <mphumier@gmail.com>
2022-07-17 19:46:17 +0200

    Add: Different routes for user

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -10,19 +10,21 @@ const corsOption = {
 // Controller imports
 const register_user = require('../controllers/registerController');
 const login_user = require('../controllers/loginController');
-const getProfile = require('../controllers/getProfile');
+const { getProfile, deleteProfile, updateProfile } = require('../controllers/getProfile');
 const verifyJWT = require('../middlewares/verifyJWT');
-const allUsers = require('../controllers/getAllUsers')
+const allUsers = require('../controllers/getAllUsers');
 const postRoutes = require('../controllers/postsController');
 
 app.use(verifyJWT);
 
 // Handle routes
 router.get('/login', login_user);
-router.get('/profile', cors(corsOption), verifyJWT, getProfile.getUserProfile);
+router.get('/profile', cors(corsOption), verifyJWT, getProfile);
 router.get('/p/posts', verifyJWT, postRoutes.getAllUserPosts);
 router.post('/login', login_user);
 router.post('/register', register_user);
 router.get('/contact', allUsers.getUsers);
+router.route('/profile/delete/:id').delete(deleteProfile);
+router.route('/profile/update/:id').put(updateProfile);
 
 module.exports = router;
