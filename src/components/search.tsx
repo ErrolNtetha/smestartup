200aeb17a7777f30277ac531073f95849a002b10
ErrolNtetha <mphumier@gmail.com>
2022-07-17 13:21:29 +0200

    Add: Clear button icon

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,20 +1,37 @@
 import React from 'react';
-import { FiSearch } from 'react-icons/fi';
+import { FiSearch, FiX } from 'react-icons/fi';
 
 interface Props {
     placeholder: string;
     children: React.ReactNode;
     searchKey: React.ChangeEventHandler<HTMLInputElement> | undefined;
+    clearSearchKey: string;
+    searchTerm: string;
 }
 
-export const Search = ({ placeholder, children, searchKey }: Props) => {
+export const Search = ({
+    placeholder,
+    children,
+    searchKey,
+    clearSearchKey,
+    searchTerm
+}: Props) => {
+    const hasResult = children[2];
+
     return (
         <section className='header__search'>
-            <section className='header__searchWrapper'>
-                <FiSearch className='header__searchIcon' />
-                <input type='text' placeholder={placeholder} onChange={searchKey} className='header__searchBar' />
+            <section className='header__searchContainer'>
+                <section className='header__searchWrapper'>
+                    <FiSearch className='header__searchIcon' />
+                    <input type='text' name='search' placeholder={placeholder} onChange={searchKey} className='header__searchBar' />
+                    {searchTerm && <FiX onClick={clearSearchKey} className='header__clearSearchKey' />}
+                </section>
+                { (hasResult && hasResult.length >= 1) && (
+                        <section className='header__searchResultContainer'>
+                            {children}
+                        </section>
+                    )}
             </section>
-            {children}
         </section>
     );
 };
