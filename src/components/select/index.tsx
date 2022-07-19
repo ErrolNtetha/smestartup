4683ee7a84d7952e487df32738c77acc72abccd5
ErrolNtetha <mphumier@gmail.com>
2022-07-19 17:34:58 +0200

    Add: Select component added

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable  jsx-a11y/click-events-have-key-events */
+import React, { useState } from 'react';
+import { FiChevronUp, FiChevronDown } from 'react-icons/fi';
 
-export const Select = () => {
+interface Props {
+    title: string;
+    children: React.ReactChild;
+}
+
+export const Select = ({ title, children }: Props) => {
+    const [open, setOpen] = useState(false);
     return (
-        <section className='select__container'>
-            <select>
-                <option> Select Option </option>
-                <option> Construction </option>
-            </select>
+        <section className='select' onClick={() => setOpen(!open)}>
+            <section className='select__selected' custom-data='true'>
+                <p> {title} </p>
+                {open
+                    ? <FiChevronUp className='select__selected__icon' />
+                    : <FiChevronDown className='select__selected__icon' />}
+            </section>
+            {open && children}
         </section>
     );
 };
