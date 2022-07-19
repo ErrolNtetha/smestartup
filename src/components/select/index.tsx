a288a55a9c9045402b7942142bcbbe8a33ac8ee6
ErrolNtetha <mphumier@gmail.com>
2022-07-19 11:34:46 +0200

    Add: Created a select component

---
+++ src/components/select/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Select = () => {
+    return (
+        <section className='select__container'>
+            <select>
+                <option> Select Option </option>
+                <option> Construction </option>
+            </select>
+        </section>
+    );
+};
