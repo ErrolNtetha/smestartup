a3a99c79fb44195601e3f486fadfaa4af8d065bd
ErrolNtetha <mphumier@gmail.com>
2022-07-20 14:09:32 +0200

    Add: Created a preview component

---
+++ src/views/suppliers/preview.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Preview = () => {
+    return (
+        <section className='supplier__previewContainer'>
+            I am a preview
+        </section>
+    );
+};
