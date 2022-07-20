28c64ad822bd310842b75f4796d41495c5ef2d85
ErrolNtetha <mphumier@gmail.com>
2022-07-20 14:13:39 +0200

    Add: ESLint disable for element interactions

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable  jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useState } from 'react';
 import { FiChevronUp, FiChevronDown } from 'react-icons/fi';
 
