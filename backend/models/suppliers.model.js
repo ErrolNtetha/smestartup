7ebb1b759551db1a8a160a94010806f68e241c80
ErrolNtetha <mphumier@gmail.com>
2022-07-20 17:28:38 +0200

    Add: Properties

--- backend/models/suppliers.model.js
+++ backend/models/suppliers.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const geocoder = require('../utils/geocode');
 
 const SuppliersSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -12,15 +13,51 @@ const SuppliersSchema = new mongoose.Schema({
         email: Array,
         other: String
     },
+    address: {
+        type: String,
+        requred: [true, 'Address cannot be empty.']
+    },
     addresses: {
         physical: {
-            name: String,
-            latitude: Number,
-            longitude: Number
+            type: {
+                type: String,
+                enum: ['Point']
+            },
+            coordinates: {
+                type: [Number],
+                index: '2dsphere'
+            },
+            country: String,
+            zip: String,
+            city: String,
+            streetName: String,
+            countryCode: String,
+            province: String,
+            formattedAddress: String
         },
         postal: String
     },
     isRegistered: Boolean,
 }, { timestamps: true });
+
+SuppliersSchema.pre('save', async function(next) {
+    const location = await geocoder.geocode(this.address);
+    console.log(location[0]);
+
+    this.addresses.physical = {
+        type: ['Point'],
+        coordinates: [location[0].longitude, location[0].latitude],
+        country: location[0].country,
+        zip: location[0].zipcode,
+        city: location[0].city,
+        countryCode: location[0].countryCode,
+        province: location[0].stateCode,
+        formattedAddress: location[0].formattedAddress
+    };
+
+    // Do not save address
+    this.address = undefined;
+    next();
+});
 
 module.exports = mongoose.model('Suppliers', SuppliersSchema);
