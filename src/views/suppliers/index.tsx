0fffeb8ab76d923f34d8a7cfe858f11347ff89a8
ErrolNtetha <mphumier@gmail.com>
2022-07-20 14:11:38 +0200

    Add: Imported Preview component

--- src/views/suppliers/index.tsx
+++ src/views/suppliers/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Filter } from './filter';
 import { Results } from './results';
+import { Preview } from './preview';
 
 export const SupplierContainer = () => {
     return (
         <section className='supplier__container'>
             <Filter />
             <Results />
+            <Preview />
         </section>
     );
 };
