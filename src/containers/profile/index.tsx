615c287461090bafa31b7184a0a867ce15ce7f94
ErrolNtetha <mphumier@gmail.com>
2022-08-05 12:25:29 +0200

    Add: Arrow button to go back

--- src/containers/profile/index.tsx
+++ src/containers/profile/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 import { Head } from 'views/profile/head';
 import { Header } from 'views/header';
 
 export const Profile = () => {
+    const history = useHistory();
+
   return (
     <>
-      <Header />
-      <section className='profile__container'>
-          <Head />
-      </section>
+        <Header>
+            <FiArrowLeft className='header__arrowLeft' onClick={() => history.goBack()} />
+        </Header>
+        <section className='profile__container'>
+            <Head />
+        </section>
     </>
   );
 };
