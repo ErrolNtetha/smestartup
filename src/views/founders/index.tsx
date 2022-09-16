c5d4452c749b8cf24cf412feafb80eb2f246c3ab
ErrolNtetha <mphumier@gmail.com>
2022-09-16 17:04:38 +0200

    Add: Imported Helmet component

--- src/views/founders/index.tsx
+++ src/views/founders/index.tsx
@@ -1,7 +1,8 @@
 import { Logo } from 'components/header/logo';
 import React from 'react';
 import { Header } from 'views/header';
-import { Helmet } from 'react-helmet-async';
+// import { Helmet } from 'react-helmet-async';
+import { HelmetTags } from 'components/helmet';
 import { useFetchData } from 'hoc/useFetchData';
 import { ScaleLoader } from 'react-spinners';
 import { Home } from './home';
@@ -14,15 +15,14 @@ export const Founder = () => {
             <Header>
                 <Logo />
             </Header>
-            <Helmet>
-                <title> Browse Co-founders for your business idea | Blendot </title>
-                <meta property='og:title' content='Find Co-founders with just few clicks' />
-                <meta property='og:type' content='article' />
-                <meta property='og:url' content='https://blendot.com/suppliers' />
-                <meta name='twitter:card' content='summary_large_image' />
-                <meta property='og:description' content='Finding suppliers for your business has never been easy. Search thousands of suppliers by location and settle your deals.' />
-                <meta property='og:site_name' content='Blendot' />
-            </Helmet>
+            <HelmetTags
+              title='Browse Co-founders for your business idea | Blendot'
+              ogTitle='Find Co-founders with just few clicks'
+              ogUrl='https://blendot.com/founders'
+              ogType='article'
+              ogSiteName='Blendot'
+              ogDescription='Meet entrepreneurs who aspires to be great with unique skills required for your business. | Blendot'
+            />
         {loading
             ? <span className='founder__loader'><ScaleLoader color='#fff' /></span>
             : data?.founders?.map((founder) => (
