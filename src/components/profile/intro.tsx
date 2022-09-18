f8e2c035c811cda92ac39e0331f62afab6947a57
ErrolNtetha <mphumier@gmail.com>
2022-09-18 11:41:57 +0200

    Add: Date and verified icon

--- src/components/profile/intro.tsx
+++ src/components/profile/intro.tsx
@@ -4,7 +4,10 @@ import { useFetchUserId } from 'hoc/useFetchUserId';
 import { Link } from 'react-router-dom';
 import { Avatar } from 'components/avatar';
 import cover from 'assets/bg/cover.png';
+import { format } from 'date-fns';
+// import { getDistance } from 'helpers/formatDistance';
 import { useSelector } from 'react-redux';
+import { MdVerified } from 'react-icons/md';
 // import { getDateInMonth } from 'helpers/formatDistance';
 
 export const Intro = () => {
@@ -14,8 +17,13 @@ export const Intro = () => {
         name,
         occupation,
         avatar,
-        _id
+        _id,
+        isVerified
     } = userData;
+    console.log(userData);
+    console.log(typeof userData.createdAt);
+    console.log(userData.createdAt);
+    // console.log(getDistance(userData.createdAt));
 
     return (
         <>
@@ -27,13 +35,12 @@ export const Intro = () => {
                     <span className='profile__personalDetails'>
                         <Avatar className='profile__avatar' avatar={avatar} />
                         <span className='profile__namesGroup'>
-                            <h3 className='profile__names'> {name.firstName} {name.lastName} </h3>
+                            <h3 className='profile__names'> {name.firstName} {name.lastName} {isVerified && <MdVerified className='profile__verifiedIcon' />} </h3>
                             <p className='profile__occupation'> {occupation} </p>
                         </span>
                     </span>
                     {userId === _id
-                        ? <Link to='/profile/edit' className='profile__editProfile'> <FiSettings /> <span>Edit Profile</span> </Link>
-                    : null}
+                        && <Link to='/profile/edit' className='profile__editProfile'> <FiSettings /> <span>Edit Profile</span> </Link> }
                 </span>
                 <span>
                     <p className='profile__bio'>
@@ -41,7 +48,7 @@ export const Intro = () => {
                         This is the bio. Just a dummy text to represent a bio of the account and visualize how it will look like. jflaskjf jfklsdjf ;asfdjfhdf dfj;dlkjf hdafljdfkld aljdfldsjfd
                     </p>
                     <p className='profile__joined'> <FiMapPin /> Durban, South Africa </p>
-                    <p className='profile__joined'> <FiClock /> Member since 7 May 2022 </p>
+                    <p className='profile__joined'> <FiClock /> Member since {format(new Date(userData.createdAt), 'd MMM u')} </p>
                 </span>
             </section>
         </>
