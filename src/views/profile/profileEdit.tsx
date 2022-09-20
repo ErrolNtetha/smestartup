56330d2d3cb05b1f763f70df35db9352a2a8b0e5
ErrolNtetha <mphumier@gmail.com>
2022-09-20 17:53:57 +0200

    Add: New component for the form to update the profile

---
+++ src/views/profile/profileEdit.tsx
@@ -0,0 +1,226 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable no-nested-ternary */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable  jsx-a11y/click-events-have-key-events */
+
+import React, { useRef } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import { occupations } from 'helpers/occupations';
+import defaultAvatar from 'assets/blendot.png';
+// import { Preview } from 'components/preview';
+// import { Avatar } from 'components/avatar';
+import { useDispatch } from 'react-redux';
+import * as Yup from 'yup';
+import { axiosPrivate } from 'config/axiosInstance';
+import { SyncLoader } from 'react-spinners';
+import { Select } from 'components/select';
+// import { Checkbox } from 'components/checkbox';
+import { useStore } from 'hoc/useStore';
+import { fetchProfile } from 'store/actions/fetchProfile';
+import { Button } from '../../components/button';
+
+export const ProfileEdit = () => {
+    const imageInput = useRef(null);
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const { userProfile: { userData } } = useStore();
+    const {
+        name,
+        avatar,
+        occupation,
+        _id,
+        bio
+    } = userData;
+
+    const traceChars = (chars: number) => chars;
+
+const title = [...occupations, 'Unemployed', 'Founder & CEO'].sort();
+
+return (
+    <section className='profile__updateContainer'>
+        <Formik
+          validationSchema={Yup.object({
+                firstName: Yup.string()
+                        .min(2, 'Your name is too short!')
+                        .max(50, 'Name is too long!')
+                        .required('First name cannot be empty.'),
+                lastName: Yup.string()
+                        .min(2, 'Your name is too short!')
+                        .max(50, 'Name is too long!')
+                        .required('Last name is required!'),
+                bio: Yup.string()
+                            .max(200, 'You have exceeded maximum character limit of 200. Remove some characters.'),
+                })}
+          initialValues={{
+                occupation,
+                firstName: name?.firstName,
+                lastName: name?.lastName,
+                location: '',
+                company: '',
+                avatar,
+                bio
+            }}
+          onSubmit={async (values) => {
+              await axiosPrivate.put(`/profile/${_id}/update`, values)
+                  .then((response) => {
+                      if (response.status === 201) {
+                            dispatch(fetchProfile(response.data.user));
+                            history.goBack();
+                      }
+                    })
+                  .catch(({ response }) => {
+                    console.error(response);
+                  });
+          }}
+        >
+          {(props) => (
+              <Form onSubmit={props.handleSubmit} className='profile__formContainer'>
+                <h4>personal</h4>
+                <hr className='global' />
+                <section className='profile__personalContainer'>
+                    {/* <section className='profile__avatarContainer'>
+                        <Avatar avatar='' className='profile__editAvatar' />
+                        <section className='profile__avatarBtns'>
+                            <Button className='profile__changeAvatar'> change avatar </Button>
+                            <Button className='profile__changeAvatar' style={{ background: 'red' }}> remove avatar </Button>
+                        </section>
+                    </section> */}
+
+                    <section className='profile__avatarContainer'>
+                        <span className='register__avatarWrapper'>
+                            <div className='register__avatarContainer' role='button' tabIndex={0} onKeyDown={() => imageInput.current.click()} onClick={() => imageInput.current.click()}>
+                                {props.values.avatar ? <img src={props.values.avatar} alt='rndo' className='register__avatar' />
+                                : <img src={defaultAvatar} alt='new user avatar' className='register__avatar' />}
+                            </div>
+                            <Button className='profile__changeAvatar'> change avatar </Button>
+                        </span>
+                    <input
+                      type='file'
+                      hidden
+                      ref={imageInput}
+                      accept='image/*'
+                      name='avatar'
+                      onChange={(e) => {
+                          const reader = new FileReader();
+                          reader.readAsDataURL(e.currentTarget.files[0]);
+
+                          reader.onload = () => {
+                              if (reader.result) {
+                                  props.setFieldValue('avatar', reader.result);
+                              }
+                          };
+                      }}
+                    />
+                    </section>
+
+                    <section>
+                        <label htmlFor='firstName'> First Name</label>
+                        <Field
+                          name='firstName'
+                          placeholder='Your First Name'
+                          className='profile__input'
+                        />
+                        {props.errors.firstName && <p style={{ color: 'red', margin: '0' }}>{props.errors.firstName}</p>}
+                    </section>
+
+                    <section>
+                        <label htmlFor='lastName'> Last Name</label>
+                        <Field
+                          name='lastName'
+                          placeholder='Your Last Name'
+                          className='profile__input'
+                        />
+                        {props.errors.lastName && <p style={{ color: 'red', margin: '0' }}>{props.errors.lastName}</p>}
+                    </section>
+
+                    <section>
+                        <label htmlFor='occupation'> Occupation </label>
+                        <Field
+                          name='occupation'
+                          placeholder='What best decribe what you do currently?'
+                          className='profile__input'
+                        />
+                    </section>
+
+                    <section>
+                    <label> Occupation </label>
+                        <Field as='select' title={!props.values.occupation ? '- Choose title -' : props.values.occupation} name='occupation' className='register__selectOccupation' component={Select}>
+                            <section className='profile__input'>
+                                {title.map((item: string) => {
+                                    return (
+                                        <section key={item}>
+                                            <hr className='select__options__optionsDivider' />
+                                            <p className='register__options__option' onClick={() => props.setFieldValue('occupation', item)}> {item} </p>
+                                        </section>
+                                    );
+                                })}
+                            </section>
+                        </Field>
+                    </section>
+
+                    <section>
+                        <label htmlFor='company'> Company </label>
+                        <Field
+                          name='company'
+                          placeholder='Name of the company you work at'
+                          className='profile__input'
+                        />
+                    </section>
+
+                    <section>
+                        <label htmlFor='bio'> Bio </label>
+                        <Field
+                          name='bio'
+                          as='textarea'
+                          rows={5}
+                          placeholder='Tell people about yourself and what makes you special...'
+                          className='profile__inputBio'
+                        />
+                        <section style={{ fontSize: '.8rem', textAlign: 'right' }}> {`${traceChars(props.values?.bio.length)}/200`} characters </section>
+                        {props.errors?.bio && <p style={{ color: 'red', margin: '0' }}>{props.errors?.bio}</p>}
+                    </section>
+
+                <hr className='global' style={{ marginTop: '3em' }} />
+                <h4> account & passwords </h4>
+                <hr className='global' />
+                    <section style={{ marginTop: '1.2em' }}>
+                        <label htmlFor='current_password'> Current Password </label>
+                        <Field
+                          name='password'
+                          type='password'
+                          placeholder='Enter your current password'
+                          className='profile__input'
+                        />
+                    </section>
+                    <section>
+                        <label htmlFor='new_password'> Create New Password </label>
+                        <Field
+                          name='password'
+                          type='password'
+                          placeholder='Enter your new password'
+                          className='profile__input'
+                        />
+                    </section>
+
+                    <section>
+                        <label htmlFor='new_password'> Confirm New Password </label>
+                        <Field
+                          name='password'
+                          type='password'
+                          placeholder='Confirm your new password'
+                          className='profile__input'
+                        />
+                    </section>
+
+                    <Button type='submit' disabled={props.isSubmitting} className='profile__button--save'>
+                        {props.isSubmitting ? <SyncLoader color='white' size={6} /> : 'save' }
+                    </Button>
+                </section>
+              </Form>
+          )}
+        </Formik>
+    </section>
+);
+};
