5f5d09e7197ea34349179a85856c2aa8394b5188
ErrolNtetha <mphumier@gmail.com>
2022-09-21 12:34:34 +0200

    Mod: Revert back to old code

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -2,18 +2,7 @@ const mongoose = require('mongoose');
 
 const userPost = new mongoose.Schema(
     {
-        author: {
-            name: {
-                firstName: { type: String, required: [true, 'The first name is required.'] },
-                lastName: { type: String, required: [true, 'The last name is required.'] }
-            },
-            id: { type: String, required: [true, 'The ID cannot be empty. Please provide the ID.'] },
-            avatar: String,
-            occupation: String,
-            isVerified: Boolean,
-            email: { type: String, lowercase: true },
-            isOwner: { type: Boolean }
-        },
+        author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
         post: String,
         encodedImage: String,
         reports: {
