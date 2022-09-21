168e911dff71c70fdfa82ff46e2b7b27e91d1b87
ErrolNtetha <mphumier@gmail.com>
2022-09-21 12:21:54 +0200

    Add: Dummy text

--- src/components/profile/body.tsx
+++ src/components/profile/body.tsx
@@ -7,6 +7,7 @@ import { useFetchData } from '../../hoc/useFetchData';
 
 export const Body = () => {
     const { data, error, loading } = useFetchData('/p/posts');
+    console.log(data);
 
     return (
         <section className='profile__bodyContainer'>
@@ -15,12 +16,18 @@ export const Body = () => {
                 ? <section className='profile__loadingContainer'> <ScaleLoader color='white' /> </section>
                 : error === 'ECONNABORTED'
                 ? <section className='profile__errorContainer'> Connection is taking longer than expected. Check your internet connection and try again. <Button className='profile__retry'> Retry </Button> </section>
-                : data.posts?.map(({ post, createdAt, _id }) => (
+                : data.posts?.map(({
+                    post,
+                    createdAt,
+                    _id,
+                    avatar
+                }) => (
                     <UserPosts
                       post={post}
                       date={createdAt}
                       id={_id}
                       key={_id}
+                      avatar={avatar}
                     />
                     ))
             }
