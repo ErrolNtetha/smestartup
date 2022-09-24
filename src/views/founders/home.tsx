7ee787dcec489c3ebd6fbeced94a6eaf17357579
ErrolNtetha <mphumier@gmail.com>
2022-09-24 06:09:35 +0200

    Changed the author to user in interface props

--- src/views/founders/home.tsx
+++ src/views/founders/home.tsx
@@ -2,17 +2,17 @@ import { Avatar } from 'components/avatar';
 import React from 'react';
 
 interface Props {
-    author: {
-        name: {
-            firstName: string;
-            lastName: string;
+    user: {
+        author: {
+            name: {
+                firstName: string;
+                lastName: string;
+            }
         }
     }
 }
 
-export const Home = ({ author }: Props) => {
-    console.log('First Name: ', author);
-
+export const Home = ({ user }: Props) => {
     return (
         <section className='founder__homeContainer'>
             <section className='founder__left' />
@@ -20,7 +20,7 @@ export const Home = ({ author }: Props) => {
                 <span className='founder__nameGroup'>
                     <Avatar avatar='' className='founder__avatar' />
                     <section className='founder__names'>
-                        <h5> {`${author.name.firstName} ${author.name.lastName}`} </h5>
+                        <h5> {`${user?.author?.name?.firstName} ${user?.author?.name?.lastName}`} </h5>
                         <p> seeking for co-founder  </p>
                         <p> 3 days ago </p>
                     </section>
@@ -32,7 +32,7 @@ export const Home = ({ author }: Props) => {
                 <hr className='global' />
                 <section className='founder__snapInfo'>
                     <p> Employment <span> Self-Employed </span> </p>
-                    <p> City <span> Durban, ZA </span> </p>
+                    <p> City <span> Durban </span> </p>
                     <p> Investment Type <span> Land & Building </span> </p>
                     <p> Education <span> Business Administation </span> </p>
                 </section>
