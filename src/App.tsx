3ced74e95f2c35cd6a0eb2bd988c429ec67b4645
ErrolNtetha <mphumier@gmail.com>
2022-09-26 14:58:08 +0200

    Add: Skeleton loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 /* eslint-disable func-names */
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { SkeletonTheme } from 'react-loading-skeleton';
 import { Main } from './routes';
+import 'react-loading-skeleton/dist/skeleton.css';
 import './scss/_index.scss';
 
 const App:React.FC = function () {
   return (
     <Router>
-      <div className='darkTheme'>
-        <Main />
-      </div>
+
+        <SkeletonTheme baseColor='#222222' highlightColor='#181818'>
+            <div className='darkTheme'>
+                <Main />
+            </div>
+        </SkeletonTheme>
     </Router>
   );
 };
