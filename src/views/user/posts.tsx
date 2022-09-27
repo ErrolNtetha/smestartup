b28206346f6f83f4cf81bc64e9d61d54e0dd7cd9
ErrolNtetha <mphumier@gmail.com>
2022-09-27 10:09:17 +0200

    Add: Post component, updated props

---
+++ src/views/user/posts.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Avatar } from 'components/avatar';
+import { FiMoreHorizontal } from 'react-icons/fi';
+import { formatDistance } from 'date-fns';
+
+interface Props {
+    post: string;
+    date: Date | number;
+    id: string;
+    author: any;
+}
+
+export const Posts = ({
+    post,
+    date,
+    id,
+    author
+}: Props) => {
+    return (
+        <section className='profile__postsContainer'>
+                <section className='profile__post' key={id}>
+                    <span className='profile__titleContainer'>
+                        <section className='profile__titleChild'>
+                            <Avatar className='profile__postAvatar' avatar={author.avatar} />
+                            <span className='profile__titleGroup' style={{ paddingLeft: '.8em' }}>
+                                <span>
+                                    <h4 className='profile__fullNames'> {author?.name?.firstName} {author?.name?.lastName} </h4>
+                                    <p className='profile__timeAgo'> {author?.occupation} </p>
+                                    <p className='profile__timeAgo'> {formatDistance(new Date(date), new Date(), { addSuffix: true })} </p>
+                                    <section />
+                                </span>
+                                <span style={{ alignSelf: 'start' }}>
+                                    <FiMoreHorizontal className='profile__ellipsis' />
+                                </span>
+                            </span>
+                        </section>
+                    </span>
+                    <p className='profile__userPost'>{post}</p>
+                    <hr className='global' />
+                </section>
+        </section>
+    );
+};
