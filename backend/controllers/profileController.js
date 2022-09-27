74324c77c5a54bd520645c11a38970d5d31f6ff2
ErrolNtetha <mphumier@gmail.com>
2022-09-27 09:46:21 +0200

    Add: Profile controller to get user data for profile viewing

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,4 +1,5 @@
 const User = require('../models/user.model');
+const Post = require('../models/post.model');
 const { cloudinary } = require('../utils/cloudinary');
 
 exports.getUserProfile = async (req, res) => {
@@ -42,4 +43,12 @@ exports.updateProfile = async (req, res) => {
         res.status(201).json({ user: updatedProfile });
     })
     .catch((err) => res.status(500).json({ err }));
+};
+
+exports.getProfile = async (req, res) => {
+    const user = await User.findById(req.params.id, 'name avatar bio isPremium isVerified createdAt occupation _id');
+    await Post.find({ author: user._id })
+        .populate('author')
+        .then((posts) => res.status(200).json({ posts, user }))
+        .catch((error) => res.status(500).json({ error }));
 };
