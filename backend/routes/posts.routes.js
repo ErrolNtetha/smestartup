67623d5ed4a66055aa0297f3f8b6217dbbfb407e
ErrolNtetha <mphumier@gmail.com>
2022-10-09 13:28:09 +0200

    Add: Refactoring

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -8,8 +8,9 @@ const verifyToken = require('../middlewares/verifyJWT');
 const deletePost = require('../controllers/deletePostController');
 
 // Handle routes
-router.get('/feed', verifyToken, postRoutes.getUserPost);
-router.post('/feed', verifyToken, postRoutes.userPosts);
+router.route('/feed')
+    .get(verifyToken, postRoutes.getUserPost)
+    .post(verifyToken, postRoutes.userPosts);
 router.put('/feed/:id', verifyToken, postRoutes.updateStars);
 router.get('/feed/p/:id', verifyToken, postRoutes.getSpecificUserPost);
 router.delete('/feed/:id', verifyToken, deletePost);
