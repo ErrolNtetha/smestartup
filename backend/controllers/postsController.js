f16ea118f813461696f58d62fe7a875087e73d68
ErrolNtetha <mphumier@gmail.com>
2022-10-09 13:06:22 +0200

    Add: Enhancements done successfully

--- backend/controllers/postsController.js
+++ backend/controllers/postsController.js
@@ -28,11 +28,15 @@ exports.incrimementLikes = async (req, res) => {
     // check if the user id already exist in array if it does, return...
     // otherwise, proceed.
     const hasStarred = await Post.findById(req.params.id);
-    if (hasStarred.stars.includes(_id)) res.status(200).json({ success: false });
-
-    await Post.findByIdAndUpdate(req.params.id, { $push: { stars: [_id] } })
-        .then(() => res.status(200).json({ success: true }))
-        .catch((error) => console.log('Ops. There was a problem.', error.message));
+    if (hasStarred.stars.includes(_id)) {
+        await Post.findByIdAndUpdate(req.params.id, { $pull: { stars: _id } })
+            .then(() => res.status(200).json({ success: true }))
+            .catch((error) => res.status(500).json({ success: false, error }));
+    } else {
+        await Post.findByIdAndUpdate(req.params.id, { $push: { stars: [_id] } })
+            .then(() => res.status(200).json({ success: true }))
+            .catch((error) => console.log('Ops. There was a problem.', error.message));
+    }
 };
 
 exports.getSpecificUserPost = async (req, res) => {
