279ffd429ab26ac3ec0384db9ab9443256588295
ErrolNtetha <mphumier@gmail.com>
2022-10-10 14:08:33 +0200

    Add: Map to profiles

--- src/views/suppliers/page/create.tsx
+++ src/views/suppliers/page/create.tsx
@@ -1,34 +1,35 @@
-import { Avatar } from 'components/avatar';
+// import { Avatar } from 'components/avatar';
 // import { Tooltip } from 'components/tooltip';
 import React from 'react';
-import { FiChevronRight } from 'react-icons/fi';
+// import { FiChevronRight } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-// import { useFetchData } from 'hoc/useFetchData';
+// import { MapProfiles } from './mapProfiles';
+import { useFetchData } from 'hoc/useFetchData';
+import { Profiles } from './profiles';
 
-interface Props {
-    id: string;
-    supplierName: string;
-    supplierPicture: string;
-}
-
-export const Create = ({ id, supplierName, supplierPicture }: Props) => {
-    //     const { data } = useFetchData('/suppliers');
+export const Create = () => {
+    const { data, loading } = useFetchData('/suppliers/my');
+    console.log(data, loading);
     return (
         <section className='supplier__createContainer'>
             <span>
                 <h5 style={{ textAlign: 'center', padding: '0', margin: '.6em 0' }}> Supplier Profiles </h5>
                 <hr className='global' />
                 <section className='supplier__supplierListContainer'>
-                    <Link to={`/suppliers/${id}/update`} className='supplier__supplierPreContainer'>
-                        <section>
-                            <Avatar avatar={supplierPicture} className='supplier__supplierPicture' />
-                            <span>{supplierName}</span>
-                        </section>
-                        <FiChevronRight />
-                    </Link>
+                    {loading
+                        ? <div> Loading </div>
+                        : data.suppliers.map((item: any) => (
+                            <Profiles
+                              key={item._id}
+                              id={item._id}
+                              supplierName={item.name}
+                              supplierPicture={item.avatar}
+                              isActive={item.isRegistered}
+                            />
+                    ))}
                 </section>
                 <span className='supplier__supplierProfiles'>
-                    <p> If you want to register another supplier profile, you will have to become a premium member to create another supplier profile. </p>
+                    <p>  </p>
                     <Link to='/suppliers/register' className='supplier__create'> register </Link>
                 </span>
             </span>
