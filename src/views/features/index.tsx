49fc33a6c5e59196500b325ce20fec506e8703f6
ErrolNtetha <mphumier@gmail.com>
2022-10-18 04:26:06 +0200

    Add: Some styles for the features and hero sections

--- src/views/features/index.tsx
+++ src/views/features/index.tsx
@@ -9,16 +9,19 @@ export const Features = () => {
         <section className='features__innerSection'>
             <section className='features__innerText'>
                 <section className='features__innerText'>
+                    <span className='features__item'>
+                        <span className='features__headerText'>
+                            <h1> Discover a new world of community for entrepreneurs in your fingertips. </h1>
+                            <h4 className='features__header'> Fancy a partnership... </h4>
+                        </span>
+                        <p className='features__description'> Collaboration over competition. How about collaborating with other entrepreneurs and find those who have what you do not have? With our founder match-maker, you will find people who have that missing puzzle of your business or idea. </p>
+                    </span>
                   <span className='features__item'>
-                    <h2 className='features__header'> Collaborate </h2>
-                    <p className='features__description'> Collaboration over competition. How about collaborating with other entrepreneurs and find those who have what you do not have? With our founder match-maker, you will find people who have that missing puzzle of your business or idea. </p>
-                  </span>
-                  <span className='features__item'>
-                    <h2 className='features__header'> Find Suppliers </h2>
+                    <h4 className='features__header'> Find Suppliers for your business </h4>
                     <p className='features__description'> Finding suppliers for your company has never been easier with our search directory. Our search tool lets you search suppliers by location so you can always get the ones that are closest to you. </p>
                   </span>
                   <span className='features__item'>
-                    <h2 className='features__header'> Newsfeed </h2>
+                    <h4 className='features__header'> Seek inspirations from peers </h4>
                     <p className='features__description'> Got some tips that you might want to share? Share with the community what is happening and what you are up to. Exchange thoughts and ideas with the like-minded entrepreneurs like you. </p>
                   </span>
                 </section>
--- src/views/hero/index.tsx
+++ src/views/hero/index.tsx
@@ -13,7 +13,7 @@ export const Hero: React.FC = () => {
     <section className='hero'>
         <section className='hero__innerHero container'>
             <aside className='hero__left'>
-                <section />
+                <section className='hero__top' />
                 <span className='hero__bottom' style={{ padding: '1em 0' }}>
                     <p className='hero__left__title'> {title} </p>
                     { !isLoggedIn ? <Button onClick={() => history.push('/register')} className='hero__left--action'> get started <FiArrowRight style={{ fontSize: '1.2rem' }} /> </Button> : <Button onClick={() => history.push('/feed')} className='hero__left--action'> go to feed <FiArrowRight style={{ fontSize: '1.2rem' }} /> </Button>}
