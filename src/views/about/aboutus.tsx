03960fc5c4fecb624964a88e5302f1cc1244e42e
ErrolNtetha <mphumier@gmail.com>
2022-10-20 18:52:34 +0200

    Add: Some changes

--- src/views/about/aboutus.tsx
+++ src/views/about/aboutus.tsx
@@ -5,11 +5,8 @@ export const AboutUs = () => {
         <section className='about__intro'>
             <section className='about__innerIntro container'>
                 <span className='about__left'>
-                <h2> Who we are, <br /> And why us...</h2>
-                <p>
-                    We are a start-up company based in <b style={{ borderBottom: '2px solid black' }}>South Africa </b>
-                    aiming to bring together business-minded people and promote SMMEs within the country and beyond.
-                </p>
+                    <h2> Who we are, <br /> And why us...</h2>
+                    <section className='about__imagePreview' />
                 </span>
             <section className='about__screenshot' />
             </section>
