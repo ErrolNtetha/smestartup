0dc8fc12d30ba667aad4fef5b201775449a383b8
ErrolNtetha <mphumier@gmail.com>
2022-10-25 17:36:46 +0200

    Add: Model for the queries

---
+++ backend/models/contact.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+    fullNames: String,
+    email: { type: String, unique: true, lowercase: true },
+    question: { type: String, require: [true, 'The question is required.'], max: 1000 }
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
+module.exports = Contact;
