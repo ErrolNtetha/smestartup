a944563f6e7e3f52ef9749bb339f7af98fd05dc8
ErrolNtetha <mphumier@gmail.com>
2022-10-26 17:10:53 +0200

    Add: Sub text

--- src/views/hero/index.tsx
+++ src/views/hero/index.tsx
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { FiArrowRight } from 'react-icons/fi';
 import { Button } from '../../components/button';
-import { title } from './typo';
+import { title, para } from './typo';
 
 export const Hero: React.FC = () => {
     const history = useHistory();
@@ -16,6 +16,7 @@ export const Hero: React.FC = () => {
                 <section className='hero__top' />
                 <span className='hero__bottom' style={{ padding: '1em 0' }}>
                     <p className='hero__left__title'> {title} </p>
+                    <p className='hero__left__para'> {para} </p>
                     { !isLoggedIn ? <Button onClick={() => history.push('/register')} className='hero__left--action'> get started <FiArrowRight style={{ fontSize: '1.2rem' }} /> </Button> : <Button onClick={() => history.push('/feed')} className='hero__left--action'> go to feed <FiArrowRight style={{ fontSize: '1.2rem' }} /> </Button>}
                     { !isLoggedIn && <Button onClick={() => history.push('/login')} className='hero__left--login'> login </Button>}
                 </span>
