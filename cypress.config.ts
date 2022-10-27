67d47897af4bdcf4963032a9e3027f121d3e99e6
ErrolNtetha <mphumier@gmail.com>
2022-10-27 09:55:47 +0200

    Add: Reconfigured Cypress

---
+++ cypress.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+    baseUrl: "http://localhost:3000",
+  },
+
+  component: {
+    setupNodeEvents(on, config) {},
+    specPattern: "src/**/*spec.{js,jsx,ts,tsx}",
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+});
