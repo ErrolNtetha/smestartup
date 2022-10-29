f83163f2ef489d83c05ca0116df4233e102edaf8
ErrolNtetha <mphumier@gmail.com>
2022-10-29 21:57:44 +0200

    Add: Error message element

--- src/views/feed/lists.tsx
+++ src/views/feed/lists.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-nested-ternary */
+
 import React from 'react';
 import { PostField } from 'components/postField';
 import { List } from 'components/lists/list';
@@ -7,18 +9,25 @@ import { useSelector } from 'react-redux';
 // import { ScaleLoader } from 'react-spinners';
 import { useFetchData } from 'hoc/useFetchData';
 import { SkeletonPosts } from 'components/skeleton';
+import { Button } from 'components/button';
 
 export const Lists = () => {
-  const toggleState = useSelector((state: RootState) => state.isToggleOn);
-    const response = useFetchData('/feed');
-    const { posts } = response.data;
+    const toggleState = useSelector((state: RootState) => state.isToggleOn);
+    const { data, loading, errorMessage } = useFetchData('/feed');
 
  return (
     <div className='feed__feedWrapper'>
         {toggleState ? <PostField /> : null}
-        { response.loading
+        { loading
                 ? <SkeletonPosts cards={10} />
-                : posts?.map(({
+                : errorMessage
+                ? (
+                    <section className='supplier__responseContainer'>
+                        <p style={{ padding: '.8em 0' }}> {errorMessage} </p>
+                        <Button className='supplier__retryButton' onClick={() => window.location.reload()}> Retry </Button>
+                    </section>
+                )
+                : data?.map(({
                     post, postImage, author, _id, createdAt, stars
                 }) => (
               <List
