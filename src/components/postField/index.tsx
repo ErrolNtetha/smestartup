753593b7a724f7f086463b4aab402b5da4e89f6d
ErrolNtetha <mphumier@gmail.com>
2022-10-29 21:55:57 +0200

    Add: Toast message on post

--- src/components/postField/index.tsx
+++ src/components/postField/index.tsx
@@ -8,16 +8,19 @@ import { axiosPrivate } from 'config/axiosInstance';
 import toggleFieldOff from 'store/actions/toggleField_OFF';
 import { Button } from 'components/button';
 import { SyncLoader } from 'react-spinners';
+import { Toast } from 'components/toast';
 
 export const PostField = () => {
-const dispatch = useDispatch();
 const [post, setPost] = useState('');
 const [images, setImages] = useState<Blob | null>(null);
 const [fileURL, setFileURL] = useState(null);
 const [loading, setLoading] = useState<boolean | null>(null);
+const [isSuccess, setIsSuccess] = useState<boolean | null>(null);
+const [isPosted, setIsPosted] = useState<boolean | null>(null);
 // const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
 const imageInput = useRef(null);
+const dispatch = useDispatch();
 
 const formData = { post, fileURL };
 
@@ -30,13 +33,17 @@ const handleSubmit = async () => {
     setLoading(true);
     await axiosPrivate.post('/feed', formData)
     .then((response) => {
+        setIsSuccess(response.data.success);
         if (response.data.success) {
+            setIsPosted(true);
             setLoading(false);
-            dispatch(toggleFieldOff());
+            // dispatch(toggleFieldOff());
         } else setLoading(false);
     })
     .catch((error) => {
         setLoading(false);
+        setIsPosted(true);
+        setIsSuccess(false);
         if (error) {
             console.log(error);
             // setErrorMessage('Something went wrong. Please try again later.');
@@ -44,28 +51,35 @@ const handleSubmit = async () => {
     });
 };
 
-useEffect(() => {
-    const reader = new FileReader();
+    useEffect(() => {
+        setTimeout(() => {
+            setIsPosted(false);
+        }, 5000);
+    }, [isPosted]);
 
-    if (images) {
-        reader.onload = (e) => {
-        const { result } = e.target;
-            if (result) {
-                setFileURL(result);
+    useEffect(() => {
+        const reader = new FileReader();
+
+        if (images) {
+            reader.onload = (e) => {
+            const { result } = e.target;
+                if (result) {
+                    setFileURL(result);
+                }
+            };
+            reader.readAsDataURL(images);
+        }
+
+        return () => {
+            if (reader && reader.readyState === 1) {
+                reader.abort();
             }
         };
-        reader.readAsDataURL(images);
-    }
-
-    return () => {
-        if (reader && reader.readyState === 1) {
-            reader.abort();
-        }
-    };
-}, [images]);
+    }, [images]);
 
   return (
     <section className='feed__postField'>
+        {isPosted && <Toast message={`${isSuccess ? 'Successfully posted.' : 'Failed to post.'}`} success={isSuccess} />}
         <section>
         <textarea
           name='post'
