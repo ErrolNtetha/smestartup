9c98b98a9917adcfa715b419d59e4949c4c9b704
ErrolNtetha <mphumier@gmail.com>
2022-10-30 14:35:03 +0200

    Mod: optional chaining

--- src/views/profile/profileEdit.tsx
+++ src/views/profile/profileEdit.tsx
@@ -171,7 +171,7 @@ return (
                           placeholder='Tell people about yourself and what makes you special...'
                           className='profile__inputBio'
                         />
-                        <section style={{ fontSize: '.8rem', textAlign: 'right' }}> {`${traceChars(props.values.bio.length)}/200`} characters </section>
+                        <section style={{ fontSize: '.8rem', textAlign: 'right' }}> {`${traceChars(props.values?.bio?.length)}/200`} characters </section>
                         {props.errors.bio && <p style={{ color: 'red', margin: '0' }}>{props.errors.bio}</p>}
                     </section>
 
