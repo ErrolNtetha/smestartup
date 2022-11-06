5d17daf6596ddf446dd743a1ea5da979dfb80f35
ErrolNtetha <mphumier@gmail.com>
2022-11-06 17:10:59 +0200

    Add: Basic test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,9 +1,15 @@
-describe('Check the login page', () => {
+describe('Login Page', () => {
     beforeEach(() => {
-        cy.visit('/')
-    })
+        cy.visit('/');
+    });
 
-    it('Renders the home page', () => {
+    it('Renders the login page', () => {
         cy.visit('/login');
-    })
-})
+
+        cy.get('[data-cy="email"]').type('mphumier@gmail.com');
+        cy.get('[data-cy="password"]').type('12345');
+        cy.get('[data-cy="submit"]').click();
+
+        cy.url().should('include', '/feed');
+    });
+});
