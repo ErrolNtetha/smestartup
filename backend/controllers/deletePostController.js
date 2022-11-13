ee01d0240c9adad2e4846eb2575cb9333717580f
ErrolNtetha <mphumier@gmail.com>
2022-11-13 08:24:20 +0200

    Add: API to delete image from Cloudinary

--- backend/controllers/deletePostController.js
+++ backend/controllers/deletePostController.js
@@ -1,19 +1,26 @@
 const Post = require('../models/post.model');
+const cloudinary = require('../utils/cloudinary.js');
 
 const deletePost = async (req, res) => {
     const { id } = req.params;
 
     await Post.findOneAndRemove({ _id: id }).exec()
-        .then((post) => {
+        .then(async (post) => {
             if (!post) {
-                console.log('No post found for that ID');
                 res.status(404).json({ success: false, message: 'Post was not found.' });
                 return;
             }
+
+            // Also delete the source image from cloudinary
+            if (post.image.public_id) {
+                console.log('Image url: ', post.encodedImage);
+                await cloudinary.v2.uploader.destroy(post.image.public_id)
+                    .then((res) => console.log('deleted the picture from cloudinary'))
+                    .catch((error) => console.log(error));
+            }
             res.json({ success: true, message: 'Post successfully deleted' });
         })
-        .catch((error) => {
-            console.log(error.message);
+        .catch(() => {
             res.status(500).json({ success: false, message: 'An error occured deleting your post.' });
         });
 };
