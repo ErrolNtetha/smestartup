a2267bbaea21903b95a0c32a7e5935bf589d88b4
ErrolNtetha <mphumier@gmail.com>
2022-11-14 18:22:37 +0200

    Add: Delete method for removing images

--- backend/controllers/deletePostController.js
+++ backend/controllers/deletePostController.js
@@ -1,28 +1,33 @@
 const Post = require('../models/post.model');
-const cloudinary = require('../utils/cloudinary.js');
+const { cloudinary } = require('../utils/cloudinary.js');
 
 const deletePost = async (req, res) => {
     const { id } = req.params;
 
-    await Post.findOneAndRemove({ _id: id }).exec()
-        .then(async (post) => {
-            if (!post) {
-                res.status(404).json({ success: false, message: 'Post was not found.' });
-                return;
-            }
+    try {
+        const post = await Post.findById(id);
+        const { signature, public_id } = post?.image; // grab the url and public id
 
-            // Also delete the source image from cloudinary
-            if (post.image.public_id) {
-                console.log('Image url: ', post.encodedImage);
-                await cloudinary.v2.uploader.destroy(post.image.public_id)
-                    .then((res) => console.log('deleted the picture from cloudinary'))
-                    .catch((error) => console.log(error));
-            }
-            res.json({ success: true, message: 'Post successfully deleted' });
-        })
-        .catch(() => {
-            res.status(500).json({ success: false, message: 'An error occured deleting your post.' });
+        if (public_id) {
+            cloudinary.uploader.destroy(public_id, signature, function(error, result) {
+                if (error) {
+                    console.log('Error: ', error);
+                    return;
+                }
+                console.log('Results: ', result);
+            });
+        }
+
+        Post.findByIdAndRemove(id, (error, result) => {
+            if (error) console.log(error)
+            res.status(200).json({
+                success: true,
+                message: 'Post successfully deleted.'
+            });
         });
+    } catch(error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
 };
 
 module.exports = deletePost;
