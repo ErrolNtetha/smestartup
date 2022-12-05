a601e30ab22a588935e9c7a708db8874d6420110
ErrolNtetha <mphumier@gmail.com>
2022-12-05 11:49:58 +0200

    Add: FAQ questions

--- src/views/faq/question.tsx
+++ src/views/faq/question.tsx
@@ -11,14 +11,20 @@ export const Questions = () => {
     const faq = [
         {
             answer: {
-                title: 'What are your objectives?',
-                question: <p className='faq__answer'>At vero eos et <a href='https://google.com/'>accusamus</a>  et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>,
+                title: 'What happens when a paid supplier profile expires?',
+                question: <p className='faq__answer'>
+                            When a paid (Pro or Premium) supplier profile expires, the profile will not be disabled but rather revert
+                            back to the &apos;Basic&apos; version. That is, you will lose previledges until you upgrade again to one of the paid plans.
+                          </p>,
             },
         },
         {
             answer: {
-                title: 'Why Blendot?',
-                question: <p className='faq__answer'>At vero eos et <a href='https://google.com/'>accusamus</a>  et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>,
+                title: 'Why should I use this website over your competitors?',
+                question: <p className='faq__answer'>
+                            We are committed and love what we are doing. Which means we are always striving to improve the user experience and how our users
+                            interact with platform. We like to go above and beyond to what our users want in the platform.
+                          </p>,
             },
         },
         {
@@ -30,19 +36,30 @@ export const Questions = () => {
         {
             answer: {
                 title: 'What happens to my supplier profile when I delete my account?',
-                question: <p className='faq__answer'>At vero eos et <a href='https://google.com/'>accusamus</a>  et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>,
+                question: <p className='faq__answer'>
+                            In the event that you delete your account, unfortunately you will lose supplier profiles administrated by you.
+                            Note that upon deleting your account, you will be reminded of this action and be asked to confirm.
+                            This, however, will be improved such that you will have the ability to transfer ownership to another user before deleting your account.
+                          </p>,
             }
         },
         {
             answer: {
                 title: 'My supplier profile was not approved, what can I do?',
-                question: <p className='faq__answer'>At vero eos et <a href='https://google.com/'>accusamus</a>  et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>,
+                question: <p className='faq__answer'>
+                            Supplier profiles to be approved they normally take up to 3 days. It might take longer than 3 days
+                            in a case where there is an infomation or action that needs to be taken from your end for us to proceed.
+                            But this information/action must have been communicated with you via email prior to the end of 3 days.
+                            If not, please send us an email at <a href='mailto:info@blendot.com'>info@blendot.com</a> and
+                            we will assist.
+                          </p>,
             }
         }
     ];
 
     return (
         <section className='faq__questionsContainer'>
+            <section> <h2> Frequently Asked Questions </h2> </section>
             <section className='container'>
                 <section className='faq__questionInner'>
                     {faq.map((item, index) => (
@@ -52,7 +69,7 @@ export const Questions = () => {
                               onClick={() => {
                                   setCurrent(index);
                                   setExpand(!expand);
-                                  console.log(current, index);
+
                                   if (index === current) {
                                       setCurrent(null);
                                       setExpand(!expand);
@@ -60,7 +77,7 @@ export const Questions = () => {
                                 }}
                             >
                                 {item.answer.title}
-                                {(index === current) ? <FiMinus /> : <FiPlus />}
+                                {(index === current) ? <FiMinus className='faq__icon' /> : <FiPlus className='faq__icon' />}
                             </p>
                             {(index === current) && item.answer.question}
                         </React.Fragment>
