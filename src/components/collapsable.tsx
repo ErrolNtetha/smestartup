db71ad020aae73a917a650802cd7f377b5a9934f
ErrolNtetha <mphumier@gmail.com>
2023-01-07 10:53:33 +0200

    feat: a prop to show an optional see less button

--- src/components/collapsable.tsx
+++ src/components/collapsable.tsx
@@ -6,18 +6,26 @@ interface IProps {
     children: string;
     end: number;
     className?: string;
+    showSeeLess?: boolean;
 }
 
-export const Collapsable = ({ children, end, className }: IProps) => {
+export const Collapsable = ({
+    children,
+    end,
+    className,
+    showSeeLess = true
+}: IProps) => {
     const text = children;
     const [seeMore, setSeeMore] = useState(true);
+    const showLess = showSeeLess && 'see less';
+
     return (
         <span className={className}>
             { seeMore && (text?.length > end) ? text.slice(0, end).concat('...') : text }
             { text?.length > end
                 && (
                     <span onClick={() => setSeeMore(!seeMore)} style={{ borderBottom: '.6px solid white', marginLeft: '.4em' }}>
-                        {seeMore ? 'see more' : 'show less'}
+                        {seeMore ? 'see more' : showLess}
                     </span>
                 )}
         </span>
