90a5bebb0983d32438760f0e826ba573c7262db3
ErrolNtetha <mphumier@gmail.com>
2023-01-15 09:07:00 +0200

    feat: added a function to toggle state

--- src/views/feed/index.tsx
+++ src/views/feed/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-// import { Followers } from './followers';
+import React, { useState } from 'react';
 import { Sidebar } from './sidebar';
 import { Lists } from './lists';
 
 export const FeedContainer = () => {
+    const [open, setOpen] = useState(false);
+    const togglePostField = () => {
+        setOpen(!open);
+    };
   return (
     <section className='feed'>
         <section className='feed__mainContent container'>
-            <Sidebar />
-            <Lists />
+            <Sidebar toggleField={togglePostField} />
+            <Lists isOpen={open} isToggled={togglePostField} />
             <section className='feed__rightColumn' />
         </section>
     </section>
