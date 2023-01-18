535f85d7115b6261f9bfeed7eedc2e0f2fe8118b
ErrolNtetha <mphumier@gmail.com>
2023-01-18 21:20:58 +0200

    refactor: added a finally block, rewrote error messages

--- src/hoc/useFetchData.tsx
+++ src/hoc/useFetchData.tsx
@@ -2,31 +2,24 @@
 
 import { useState, useEffect } from 'react';
 import { axiosPrivate } from 'config/axiosInstance';
-// import { useHistory } from 'react-router-dom';
 
 export const useFetchData = (url: string) => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [errorMessage, setError] = useState<string | null>(null);
-    // const history = useHistory();
 
     useEffect(() => {
         const fetchData = async () => {
            await axiosPrivate.get(url)
                 .then((res) => {
+                    setError(res.request);
                     setData(res?.data);
-                    setLoading(false);
                 })
                 .catch((error) => {
-                    setLoading(false);
-                    if (error.response) {
-                        setError('Your session has expired. Please login again.');
-                    }
-                        else if (error.request) setError('Something went wrong. Please try again later.');
-                    else {
-                        setError('Something just went wrong. Please try again later.');
-                    }
-            });
+                    if (error.response) setError('Something went wrong. Please try again later.');
+                    else setError('Something went wrong. Please try again later.');
+                })
+                .finally(() => setLoading(false));
         };
         fetchData();
     }, [url]);
