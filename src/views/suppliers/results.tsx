9becd81488a88a713c34f33bf3be92229bfddcd3
ErrolNtetha <mphumier@gmail.com>
2023-01-18 21:28:17 +0200

    refactor: added a error message text

--- src/views/suppliers/results.tsx
+++ src/views/suppliers/results.tsx
@@ -22,7 +22,7 @@ export const Results = () => {
                 : errorMessage
                 ? (
                     <section className='supplier__responseContainer'>
-                        <p>There was a problem with our severs. Please try again later.</p>
+                        <p>{errorMessage}</p>
                         <Button className='supplier__retryButton' onClick={() => window.location.reload()}> Retry </Button>
                     </section>
                 )
