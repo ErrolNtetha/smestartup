775b3a863e66a2ebe2f90e54bee3249651578666
ErrolNtetha <mphumier@gmail.com>
2023-02-10 08:00:22 +0200

    feat: added order history

--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -1,3 +1,5 @@
+/* eslint-disable func-names */
+
 const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
@@ -7,7 +9,7 @@ const orderSchema = mongoose.Schema({
         enum: ['starter', 'pro', 'premium'],
         required: [true, 'Plan type is required.']
     },
-    payfastTransactionId: {
+    paymentId: {
         type: String,
         required: [true, 'Payfast transaction ID is required.']
     },
@@ -15,7 +17,30 @@ const orderSchema = mongoose.Schema({
         type: Date,
         required: [true, 'Next payment date is required.']
     },
+    orderHistory: []
 }, { timestamps: true });
 
 const order = mongoose.model('Order', orderSchema);
 module.exports = order;
+
+orderSchema.pre('save', function () {
+    const s = this;
+    if (s.paymentId && s.paymentPlan) {
+        s.orderHistory.push({
+            author: s.author,
+            paymetntPlan: s.paymentPlan,
+            paymentId: s.pf_payment_id,
+            merchantId: s.merchant_id,
+            firstName: s.name_first,
+            lastName: s.name_last,
+            emailAddress: s.email_address,
+            itemName: s.item_name,
+            transaction: {
+                amountGross: s.amount_gross,
+                amountFee: s.amount_fee,
+                amountNet: s.amount_net,
+                status: s.payment_status
+            }
+        });
+    }
+});
