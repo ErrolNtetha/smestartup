5151681f919dfbe855262fe0fde2a0711bc1f929
ErrolNtetha <mphumier@gmail.com>
2023-03-25 12:01:48 +0200

    refactor: added few items and changed starter plan

--- src/views/suppliers/page/register/offers.tsx
+++ src/views/suppliers/page/register/offers.tsx
@@ -5,7 +5,7 @@ interface Price {
     price: number;
 }
 
-export const Starter = () => {
+export const Free = () => {
     return (
         <section className='supplier__planInfo'>
             <section style={{ width: '100%' }}>
@@ -23,6 +23,41 @@ export const Starter = () => {
                         </li>
                         <li>
                             <FiCheck /> Upload 1 image
+                        </li>
+                    </ul>
+                </section>
+            </section>
+        </section>
+    );
+};
+
+export const Starter = ({ price }: Price) => {
+    return (
+        <section className='supplier__planInfo'>
+            <section style={{ width: '100%' }}>
+                <hr className='global' />
+                <span className='supplier__planTitle'>
+                    <p>R{price}</p>
+                    <p className='supplier__billingInterval'> Monthly </p>
+                </span>
+                <hr className='global' />
+                <section className='supplier__offerDetails'>
+                    <p style={{ margin: '0', paddingBottom: '4px' }}> What you get: </p>
+                    <ul>
+                        <li>
+                            <FiCheck /> Map View
+                        </li>
+                        <li>
+                            <FiCheck /> Basic contacts
+                        </li>
+                        <li>
+                            <FiCheck /> 1 branch
+                        </li>
+                        <li>
+                            <FiCheck /> Up to 3 images
+                        </li>
+                        <li>
+                            <FiCheck /> Email Support
                         </li>
                     </ul>
                 </section>
@@ -51,7 +86,7 @@ export const Pro = ({ price }: Price) => {
                             <FiCheck /> Additional contact info
                         </li>
                         <li>
-                            <FiCheck /> Up to 2 branches
+                            <FiCheck /> Up to 3 branches
                         </li>
                         <li>
                             <FiCheck /> Social media links
@@ -61,6 +96,9 @@ export const Pro = ({ price }: Price) => {
                         </li>
                         <li>
                             <FiCheck /> Up to 3 tags
+                        </li>
+                        <li>
+                            <FiCheck /> Claim verified badge
                         </li>
                         <li>
                             <FiCheck /> Email support
