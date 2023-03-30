0c67696fc42005e0597abf2a387b5443918198a0
ErrolNtetha <mphumier@gmail.com>
2023-03-30 22:17:28 +0200

    refactor: changed the whole section, new look

--- src/views/features/index.tsx
+++ src/views/features/index.tsx
@@ -1,27 +1,53 @@
 import React from 'react';
+import { FiCheckCircle } from 'react-icons/fi';
+import shoot from '../../assets/photoShoot.jpg';
 
 export const Features = () => {
   return (
     <section className='features'>
-        <section className='features__innerSection'>
-            <section className='features__innerText'>
-                <section className='features__innerText'>
-                    <span className='features__item'>
-                        <span className='features__headerText'>
-                            <h1> Discover a new world of community for entrepreneurs in your fingertips. </h1>
-                            <h4 className='features__header'> Fancy a partnership... </h4>
-                        </span>
-                        <p className='features__description'> Collaboration over competition. How about collaborating with other entrepreneurs and find those who have what you do not have? With our founder match-maker, you will find people who have that missing puzzle of your business or idea. </p>
-                    </span>
-                  <span className='features__item'>
-                    <h4 className='features__header'> Find Suppliers for your business </h4>
-                    <p className='features__description'> Finding suppliers for your company has never been easier with our search directory. Our search tool lets you search suppliers by location so you can always get the ones that are closest to you. </p>
-                  </span>
-                  <span className='features__item'>
-                    <h4 className='features__header'> Seek inspirations from peers </h4>
-                    <p className='features__description'> Got some tips that you might want to share? Share with the community what is happening and what you are up to. Exchange thoughts and ideas with the like-minded entrepreneurs like you. </p>
-                  </span>
+        <section className='features__innerSection container'>
+            <div className='features__header'>
+                <h3> Discover a <span>South Africa</span>&apos;s new world of community for entrepreneurs in your fingertips. </h3>
+                <p>
+                    Gather with entrepreneurs, search for suppliers, find business partners and share your journey.
+
+                </p>
+            </div>
+            <section className='features__featuresListContainer'>
+                <section className='features__feature'>
+                    <span> 01 </span>
+                    <h4> Find reliable, verified suppliers for your business. </h4>
+                    <p> Transact with confidence with verified suppliers across the country. Our suppliers are vetted upon listing and differentiated from others. </p>
                 </section>
+
+                <section className='features__feature'>
+                    <span> 02 </span>
+                    <h4> Take inspiration and make friends with other entrepreneurs like you. </h4>
+                    <p> Stay on the loop with our <strong> Newsfeed </strong> and see what other entrepreneurs are up to during the day, get motivated and get going. </p>
+                </section>
+
+                <section className='features__feature'>
+                    <span> 03 </span>
+                    <h4> Fancy a partnership? Browse business partners. </h4>
+                    <p> Businesses are never meant to be a lonely journey. Find complimentary skills with people who are passionate as you and start working on that business idea that keeps you awake at night! </p>
+                </section>
+            </section>
+            <section className='features__showCaseContainer'>
+                <aside className='features__imageContainer'>
+                    <img src={shoot} alt='Fresh vegetables' className='features__veges' />
+                </aside>
+                <aside className='features__asideRight'>
+                    <h4> Give your business a head-start with our <span>verified</span> suppliers. </h4>
+                    <p> When supplying food products in South Africa, manufacturers must meet a number of compliance requirements set out by the country&apos;s food safety regulatory bodies. These requirements are put in place to ensure that food products are safe for consumption and meet certain quality standards. When determining if a supplier is trustworthy there are several key factors that should be considered. </p>
+                    <section className='features__listPoints'>
+                        <p> Our suppliers embrace: </p>
+                        <div> <FiCheckCircle /> Transparency </div>
+                        <div> <FiCheckCircle /> Compliance </div>
+                        <div> <FiCheckCircle /> Quality </div>
+                        <div> <FiCheckCircle /> Professionalism </div>
+                        <div> <FiCheckCircle /> Reliability </div>
+                    </section>
+                </aside>
             </section>
         </section>
     </section>
