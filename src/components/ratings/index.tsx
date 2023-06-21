b095d9c80097a869f076bd7839127fe78e89f6f5
ErrolNtetha <mphumier@gmail.com>
2023-06-21 09:23:21 +0200

    feat: passed onChange prop

--- src/components/ratings/index.tsx
+++ src/components/ratings/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactStars from 'react-rating-stars-component';
 
-export const Rating = () => {
+interface Props {
+    onRateChange: React.ChangeEventHandler;
+}
+
+export const Rating = ({ onRateChange }: Props) => {
     return (
-        <div>
-            <ReactStars
-              count={5}
-              size={15}
-            />
-        </div>
+        <ReactStars
+          count={5}
+          size={15}
+          onChange={onRateChange}
+        />
     );
 };
