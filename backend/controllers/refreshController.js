35a8d713c4853b49cd0f12d3d7d1d1a3624e97a1
ErrolNtetha <mphumier@gmail.com>
2023-06-25 17:42:01 +0200

    refactor(api): reduced token duration for testing

--- backend/controllers/refreshController.js
+++ backend/controllers/refreshController.js
@@ -1,19 +1,20 @@
 const jwt = require('jsonwebtoken');
+const Users = require('../models/user.model');
 
 // create a new access token and send it to the client
-exports.refreshToken = (req, res, next) => {
-    const { id, email } = req.user;
+exports.refreshToken = async (req, res) => {
+    console.log('user id: ', req.user.id);
+    const user = await Users.find({ email: req.user.email });
 
     const payload = {
-        id,
-        email
+        id: user._id,
+        email: user.email
     };
 
-    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '5d' });
     try {
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '15s' });
         res.status(200).json({ accessToken: `Bearer ${accessToken}` });
-    } catch (err) {
-        res.status(500).json({ success: false, err });
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
     }
-    next();
 };
